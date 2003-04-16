44fce14a0a9c76e5041425859758f9ca467a6960
crotwell <crotwell@localhost>
2003-04-16 09:46:02 -0400

    [svn r3694] change to use native fft from apple

--- src/edu/sc/seis/receiverFunction/IterDecon.java
+++ src/edu/sc/seis/receiverFunction/IterDecon.java
@@ -10,7 +10,7 @@ import edu.sc.seis.fissuresUtil.freq.*;
  * Created: Sat Mar 23 18:24:29 2002
  *
  * @author <a href="mailto:">Philip Crotwell</a>
- * @version $Id: IterDecon.java 3674 2003-04-13 02:15:09Z crotwell $
+ * @version $Id: IterDecon.java 3694 2003-04-16 13:46:02Z crotwell $
  */
 
 public class IterDecon {
@@ -33,8 +33,8 @@ public class IterDecon {
         /* Now begin the cross-correlation procedure
            Put the filter in the signals
         */
-        float[] f  = gaussianFilter(numerator, gwidthFactor, dt);
-        float[] g  = gaussianFilter(denominator, gwidthFactor, dt);
+        float[] f  = gaussianFilter(makePowerTwo(numerator), gwidthFactor, dt);
+        float[] g  = gaussianFilter(makePowerTwo(denominator), gwidthFactor, dt);
 
         // compute the power in the "numerator" for error scaling
         float fPower = power(f);
@@ -57,7 +57,7 @@ public class IterDecon {
                 shifts[bump] = getMaxIndex(corr);
             } // end of else
             amps[bump] = corr[shifts[bump]]; // note don't normalize by dt here
-            System.out.println("Corr max is "+amps[bump]+" at index "+shifts[bump]+" for length "+g.length);
+            System.out.println("Corr max is "+amps[bump]+" at index "+shifts[bump]+" for length "+g.length+" with dt="+dt);
 
             predicted = buildDecon(amps, shifts, g.length, gwidthFactor, dt);
             float[] predConvolve = Cmplx.convolve(predicted, denominator);
@@ -136,7 +136,7 @@ public class IterDecon {
 
     public static int getMinIndex(float[] data) {
         int index = 0;
-        for (int i=1; i<data.length; i++) {
+        for (int i=1; i<data.length/2; i++) {
             if (data[i] < data[index]) {
                 index = i;
             } 
@@ -146,7 +146,7 @@ public class IterDecon {
 
     public static int getMaxIndex(float[] data) {
         int index = 0;
-        for (int i=1; i<data.length; i++) {
+        for (int i=1; i<data.length/2; i++) {
             if (data[i] > data[index]) {
                 index = i;
             }
@@ -176,6 +176,82 @@ public class IterDecon {
     public static float[] gaussianFilter(float[] x, 
                                          float gwidthFactor, 
                                          float dt) {
+        float[] forward = new float[x.length];
+        System.arraycopy(x, 0, forward, 0, x.length);
+        NativeFFT.forward(forward);
+
+        double df = 1/(forward.length * dt);
+        double d_omega = 2*Math.PI*df;
+        double gwidth = 4*gwidthFactor*gwidthFactor;
+        double gauss;
+        double omega;
+
+        // Handle the nyquist frequency
+        omega = Math.PI/dt; // eliminate 2 / 2
+        gauss = Math.exp(-omega*omega / gwidth);
+        forward[1] *= gauss;
+
+        int j;
+        for (int i=1; i<forward.length/2; i++) {
+            j  = i*2;
+            omega = i*d_omega;
+            gauss = Math.exp(-omega*omega / gwidth);
+            forward[j] *= gauss;
+            forward[j+1] *= gauss;
+        }
+	
+        NativeFFT.inverse(forward);
+        
+        return forward;
+    }
+
+    public static float[] phaseShift(float[] x, float inShift, float dt) {
+        // native fft has imag part with opposite sign ???
+        float shift = -1 * inShift; 
+
+        int n2 = nextPowerTwo(x.length);
+        int halfpts = n2 / 2;
+
+        float[] forward = new float[x.length];
+        System.arraycopy(x, 0, forward, 0, x.length);
+        NativeFFT.forward(forward);
+
+        double df = 1/(forward.length * dt);
+        double d_omega = 2*Math.PI*df;
+
+        double omega;
+        //Handle the nyquist frequency
+        omega = Math.PI/dt;
+        forward[1] *= (float)Math.cos(omega*shift);
+        //System.out.println("nyquist  omega="+omega);
+
+        double a,b,c,d;
+        for (int j=2; j<forward.length-1; j+=2) {
+            omega = (j/2)*d_omega;
+            //  System.out.print(j/2+"  omega="+omega+" f="+forward[j]+" "+forward[j+1]);
+            a = forward[j];
+            b = forward[j+1];
+            c = Math.cos(omega*shift);
+            d = Math.sin(omega*shift);
+
+            forward[j] = (float)(a*c-b*d);
+            forward[j+1] = (float)(a*d+b*c);
+            //System.out.println(" after f="+forward[j]+" "+forward[j+1]);
+        }
+	
+        NativeFFT.inverse(forward);
+
+        return forward;
+    }
+
+    /** convolve a function with a unit-area Gaussian filter.
+     *  The 1D gaussian is: f(x) = 1/(2*PI*sigma) e^(-x^2/(q * sigma^2))
+     *  and the impluse response is: g(x) = 1/(2*PI)e^(-sigma^2 * u^2 / 2)
+     * 
+     */
+    public static float[] oldgaussianFilter(float[] x, 
+                                            float gwidthFactor, 
+                                            float dt) {
         Cmplx[] forward = Cmplx.fft(x);
 
         double df = 1/(forward.length * dt);
@@ -184,11 +260,11 @@ public class IterDecon {
         double gauss;
         double omega;
 
-//         // Handle the nyquist frequency
-//         omega = Math.PI/dt; // eliminate 2 / 2
-//         gauss = Math.exp(-omega*omega / gwidth);
+        //         // Handle the nyquist frequency
+        //         omega = Math.PI/dt; // eliminate 2 / 2
+        //         gauss = Math.exp(-omega*omega / gwidth);
 
-//         forward[0].i *= gauss;
+        //         forward[0].i *= gauss;
 
         for (int i=1; i<forward.length; i++) {
             omega = i*d_omega;
@@ -199,15 +275,15 @@ public class IterDecon {
 	
         float[] ans = Cmplx.fftInverse(forward, x.length);
         
-//         float scaleFactor = (float)(dt * 2 * df);
-//         for (int i=0; i<ans.length; i++) {
-//             ans[i] *= scaleFactor;
-//         }
+        //         float scaleFactor = (float)(dt * 2 * df);
+        //         for (int i=0; i<ans.length; i++) {
+        //             ans[i] *= scaleFactor;
+        //         }
 
         return ans;
     }
 
-    public static float[] phaseShift(float[] x, float shift, float dt) {
+    public static float[] oldphaseShift(float[] x, float shift, float dt) {
         int n2 = nextPowerTwo(x.length);
         int halfpts = n2 / 2;
 
@@ -225,6 +301,7 @@ public class IterDecon {
 
         for (int i=1; i<forward.length; i++) {
             omega = i*d_omega;
+            System.out.print(i+"  omega="+omega+" f="+forward[i].r+" "+forward[i].i);
             a = forward[i].r;
             b = forward[i].i;
             c = Math.cos(omega*shift);
@@ -232,6 +309,7 @@ public class IterDecon {
 
             forward[i].r = a*c-b*d;
             forward[i].i = a*d+b*c;
+            System.out.println(" after f="+forward[i].r+" "+forward[i].i);
         }
 	
         float[] ans = Cmplx.fftInverse(forward, x.length);
@@ -241,6 +319,12 @@ public class IterDecon {
         //            ans[i] *= scaleFactor;
         //        }
         return ans;
+    }
+
+    public static float[] makePowerTwo(float[] data) {
+        float[] out = new float[nextPowerTwo(data.length)];
+        System.arraycopy(data, 0, out, 0, data.length);
+        return out;
     }
 
     public static int nextPowerTwo(int n) {

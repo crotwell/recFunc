78043c10b14d6cca9b9e4cb24a11d3b792892be1
crotwell <crotwell@localhost>
2004-05-01 10:07:11 -0400

    [svn r8441] add scepp2003.xml to dist

--- recFuncDistBuilder.py
+++ recFuncDistBuilder.py
@@ -1,12 +1,14 @@
 import sys, os, time
 sys.path.append("../devTools/maven")
 sys.path.append("./scripts")
+sys.path.append("../sod/scripts")
 import distBuilder, sodScriptBuilder, ProjectParser
 
 def buildDist(proj, name=None):
     if not os.path.exists('scripts/logs'): os.mkdir('scripts/logs')
-    extras = [('('sodRF/recfuncsod.sh', 'recfuncsod.sh'),
-              ('scripts/cwg.prop', 'cwg.prop'),
+    extras = [('sodRF/recfuncsod.sh', 'recfuncsod.sh'),
+              ('sodRF/scepp2003.xml', 'scepp2003.xml'),
+              ('../sod/scripts/cwg.prop', 'cwg.prop'),
               ('scripts/logs', 'logs', False)]
     scripts = sodScriptBuilder.buildAll(proj)
     if name is None: name = proj.name + '-' + time.strftime('%y%m%d')

f5ca7f917e7dc9f32298512e8a18e8a5df3cefb3
crotwell <crotwell@localhost>
2004-08-12 21:10:34 -0400

    [svn r10166] initial

---
+++ src/edu/sc/seis/receiverFunction/crust2/Crust2Process.java
@@ -0,0 +1,56 @@
+/**
+ * Crust2Process.java
+ *
+ * @author Created by Omnicore CodeGuide
+ */
+
+package edu.sc.seis.receiverFunction.crust2;
+
+import edu.iris.Fissures.IfEvent.EventAccessOperations;
+import edu.iris.Fissures.IfSeismogramDC.RequestFilter;
+import edu.iris.Fissures.Location;
+import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
+import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
+import edu.sc.seis.sod.ChannelGroup;
+import edu.sc.seis.sod.CookieJar;
+import edu.sc.seis.sod.process.waveformArm.WaveformVectorProcess;
+import edu.sc.seis.sod.process.waveformArm.WaveformVectorResult;
+import edu.sc.seis.sod.status.StringTreeLeaf;
+import java.io.IOException;
+import org.w3c.dom.Element;
+
+public class Crust2Process implements WaveformVectorProcess  {
+    public Crust2Process(Element config) {
+    }
+
+    public WaveformVectorResult process(EventAccessOperations event,
+                                        ChannelGroup channelGroup,
+                                        RequestFilter[][] original,
+                                        RequestFilter[][] available,
+                                        LocalSeismogramImpl[][] seismograms,
+                                        CookieJar cookieJar) throws Exception {
+        if (crust2 != null) {
+            Location loc = channelGroup.getChannels()[0].my_site.my_station.my_location;
+            Crust2Profile profile = crust2.getClosest(loc.longitude,
+                                                      loc.latitude);
+            cookieJar.put("Crust2_H", new Float(profile.getLayer(7).topDepth));
+            cookieJar.put("Crust2_Vp", new Float(profile.getPWaveAvgVelocity()));
+            cookieJar.put("Crust2_Vs", new Float(profile.getSWaveAvgVelocity()));
+            cookieJar.put("Crust2_VpVs", new Float(profile.getPWaveAvgVelocity() /
+                                                       profile.getSWaveAvgVelocity()));
+        }
+        return new WaveformVectorResult(seismograms, new StringTreeLeaf(this, true));
+
+    }
+
+    transient static Crust2 crust2 = null;
+    static {
+        try {
+            crust2 = new Crust2();
+        } catch (IOException e) {
+            GlobalExceptionHandler.handle("Couldn't load Crust2.0", e);
+        }
+    }
+
+}
+

aafe1c12c13dfd9c60b7cd97677cef61b2a290f0
crotwell <crotwell@localhost>
2004-09-16 13:27:04 -0400

    [svn r10565] Here a StringTree, there a StringThree, Everywhere a StringTree, Old MacSod had a farm, EIEIO

--- src/edu/sc/seis/receiverFunction/server/RecFuncCacheCheck.java
+++ src/edu/sc/seis/receiverFunction/server/RecFuncCacheCheck.java
@@ -14,6 +14,8 @@ import edu.sc.seis.sod.ChannelGroup;
 import edu.sc.seis.sod.CommonAccess;
 import edu.sc.seis.sod.ConfigurationException;
 import edu.sc.seis.sod.CookieJar;
+import edu.sc.seis.sod.status.StringTree;
+import edu.sc.seis.sod.status.StringTreeLeaf;
 import edu.sc.seis.sod.subsetter.eventChannel.vector.EventVectorSubsetter;
 
 
@@ -38,14 +40,14 @@ public class RecFuncCacheCheck implements EventVectorSubsetter {
     /**
      * returns true if the receiver functions are already cached.
      */
-    public boolean accept(EventAccessOperations event,
+    public StringTree accept(EventAccessOperations event,
                           ChannelGroup channel,
                           CookieJar cookieJar) throws Exception {
         ChannelId[] chanId = new ChannelId[channel.getChannels().length];
         for(int i = 0; i < chanId.length; i++) {
             chanId[i] = channel.getChannels()[i].get_id();
         }
-        return cache.isCached(event.get_preferred_origin(), chanId, deconConfig);
+        return new StringTreeLeaf(this, cache.isCached(event.get_preferred_origin(), chanId, deconConfig));
     }
     
     RecFuncCacheOperations cache;

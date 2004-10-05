823b930ba376d945005c1e8e266a3bbc68dcbb7f
crotwell <crotwell@localhost>
2004-10-05 15:35:13 -0400

    [svn r10784] initial

---
+++ src/edu/sc/seis/receiverFunction/server/NetworkDBPopulate.java
@@ -0,0 +1,28 @@
+package edu.sc.seis.receiverFunction.server;
+
+import java.sql.SQLException;
+import edu.iris.Fissures.IfNetwork.NetworkAccess;
+import edu.iris.Fissures.IfNetwork.NetworkDCOperations;
+import edu.iris.Fissures.IfNetwork.NetworkNotFound;
+import edu.sc.seis.fissuresUtil.cache.BulletproofVestFactory;
+import edu.sc.seis.fissuresUtil.database.network.JDBCNetwork;
+import edu.sc.seis.fissuresUtil.namingService.FissuresNamingService;
+import edu.sc.seis.fissuresUtil.simple.Initializer;
+
+/**
+ * @author crotwell Created on Oct 5, 2004
+ */
+public class NetworkDBPopulate {
+
+    public static void main(String[] args) throws NetworkNotFound, SQLException {
+        Initializer.init(args);
+        FissuresNamingService fisName = Initializer.getNS();
+        NetworkDCOperations netDC = BulletproofVestFactory.vestNetworkDC("edu/iris/dmc",
+                                                                         "IRIS_NetworkDC_1.0",
+                                                                         fisName);
+        NetworkAccess[] nets = netDC.a_finder().retrieve_by_code(args[0]);
+        JDBCNetwork jdbcNet = new JDBCNetwork();
+        jdbcNet.put(nets[0].get_attributes());
+        System.out.println("Done "+args[0]);
+    }
+}

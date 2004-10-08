86fca4dae66bc703440613412e62fef5461a7375
crotwell <crotwell@localhost>
2004-10-08 10:31:41 -0400

    [svn r10836] mkdirs

--- src/edu/sc/seis/receiverFunction/server/StackSummary.java
+++ src/edu/sc/seis/receiverFunction/server/StackSummary.java
@@ -22,59 +22,77 @@ import edu.sc.seis.receiverFunction.SumHKStack;
 import edu.sc.seis.sod.ConfigurationException;
 import edu.sc.seis.sod.status.FissuresFormatter;
 
-
 /**
- * @author crotwell
- * Created on Oct 7, 2004
+ * @author crotwell Created on Oct 7, 2004
  */
 public class StackSummary {
 
-    public StackSummary(Connection conn) throws IOException, SQLException, ConfigurationException, TauModelException {
+    public StackSummary(Connection conn) throws IOException, SQLException,
+            ConfigurationException, TauModelException {
         jdbcHKStack = new JDBCHKStack(conn);
     }
-    
-    public void createSummary(String net, File parentDir, float minPercentMatch) throws FissuresException, NotFound, IOException, SQLException {
-        JDBCStation jdbcStation = jdbcHKStack.getJDBCChannel().getSiteTable().getStationTable();
+
+    public void createSummary(String net, File parentDir, float minPercentMatch)
+            throws FissuresException, NotFound, IOException, SQLException {
+        JDBCStation jdbcStation = jdbcHKStack.getJDBCChannel()
+                .getSiteTable()
+                .getStationTable();
         JDBCNetwork jdbcNetwork = jdbcStation.getNetTable();
         NetworkId[] netId = jdbcNetwork.getAllNetworkIds();
         for(int i = 0; i < netId.length; i++) {
-            if (netId[i].network_code.equals(net)) {
+            if(netId[i].network_code.equals(net)) {
                 Station[] station = jdbcStation.getAllStations(netId[i]);
                 for(int j = 0; j < station.length; j++) {
-                    createSummary(station[j].get_id(), parentDir, minPercentMatch);
+                    createSummary(station[j].get_id(),
+                                  parentDir,
+                                  minPercentMatch);
                 }
             }
         }
     }
-    
-    public void createSummary(StationId station, File parentDir, float minPercentMatch) throws FissuresException, NotFound, IOException, SQLException {
-        SumHKStack sumStack = jdbcHKStack.sum(station.network_id.network_code, station.station_code, minPercentMatch);
-        if (sumStack == null) { 
-            logger.info("No hk plots for "+StationIdUtil.toStringNoDates(station)+" with match > "+minPercentMatch);
-            return; 
+
+    public void createSummary(StationId station,
+                              File parentDir,
+                              float minPercentMatch) throws FissuresException,
+            NotFound, IOException, SQLException {
+        SumHKStack sumStack = jdbcHKStack.sum(station.network_id.network_code,
+                                              station.station_code,
+                                              minPercentMatch);
+        if(sumStack == null) {
+            logger.info("No hk plots for "
+                    + StationIdUtil.toStringNoDates(station) + " with match > "
+                    + minPercentMatch);
+            return;
         }
         BufferedImage image = sumStack.createStackImage();
-        
-        File outSumImageFile  = new File(parentDir,"SumHKStack_"+minPercentMatch+"_"+FissuresFormatter.filize(ChannelIdUtil.toStringNoDates(sumStack.getChannel().get_id())+".png"));
-        if (outSumImageFile.exists()) {outSumImageFile.delete();}
+        parentDir.mkdirs();
+        File outSumImageFile = new File(parentDir,
+                                        "SumHKStack_"
+                                                + minPercentMatch
+                                                + "_"
+                                                + FissuresFormatter.filize(ChannelIdUtil.toStringNoDates(sumStack.getChannel()
+                                                        .get_id())
+                                                        + ".png"));
+        if(outSumImageFile.exists()) {
+            outSumImageFile.delete();
+        }
         javax.imageio.ImageIO.write(image, "png", outSumImageFile);
     }
-    
+
     JDBCHKStack jdbcHKStack;
- 
+
     private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(StackSummary.class);
-    
+
     public static void main(String[] args) {
-    try {
-        Properties props = RecFuncCacheStart.loadProps(args);
-        ConnMgr.setDB(ConnMgr.POSTGRES);
-        Connection conn = ConnMgr.createConnection();
-        StackSummary summary = new StackSummary(conn);
-        summary.createSummary(args[0], new File("stackImages"), 90f);
-    } catch(Exception e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-    }
-    
+        try {
+            Properties props = RecFuncCacheStart.loadProps(args);
+            ConnMgr.setDB(ConnMgr.POSTGRES);
+            Connection conn = ConnMgr.createConnection();
+            StackSummary summary = new StackSummary(conn);
+            summary.createSummary(args[0], new File("stackImages"), 90f);
+        } catch(Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }

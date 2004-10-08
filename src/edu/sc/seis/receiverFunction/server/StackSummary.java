265eeedae5775e441d4d9de81cfba352a1f0145e
crotwell <crotwell@localhost>
2004-10-08 12:45:41 -0400

    [svn r10840] newline

--- src/edu/sc/seis/receiverFunction/server/StackSummary.java
+++ src/edu/sc/seis/receiverFunction/server/StackSummary.java
@@ -75,6 +75,7 @@ public class StackSummary {
                     }
                     
                     textSumm.write(outStr);
+                    textSumm.newLine();
                 }
             }
         }

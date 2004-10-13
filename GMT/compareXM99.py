91afac46c1f3403b33918daa6ce843d787a879a8
crotwell <crotwell@localhost>
2004-10-13 17:35:45 -0400

    [svn r10885] fix split on multiple spaces

--- GMT/compareXM99.py
+++ GMT/compareXM99.py
@@ -3,7 +3,7 @@ f=open('../DavidWilson/ristrastationvels.txt', 'r')
 lines = f.readlines()
 dwilson = {}
 for l in lines:
-  s = l.split(' ')
+  s = l.split()
   dwilson["XM."+s[0]] = s[5]
 f.close()
 
@@ -11,10 +11,11 @@ f=open('depth_vpvs.txt', 'r')
 lines = f.readlines()
 ears = {}
 for l in lines:
-  s = l.split(' ')
+  s = l.split()
   ears[s[0]] = s[3]
 f.close()
 
+print "EARS Wilson Station"
 for sta in dwilson.keys():
   print ears[sta], dwilson[sta], sta
 

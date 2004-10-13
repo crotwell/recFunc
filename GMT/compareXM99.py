5b077a3c53c5dc30b2e0bd00050a831d8e925952
crotwell <crotwell@localhost>
2004-10-13 17:22:08 -0400

    [svn r10884] initial

---
+++ GMT/compareXM99.py
@@ -0,0 +1,22 @@
+
+f=open('../DavidWilson/ristrastationvels.txt', 'r')
+lines = f.readlines()
+dwilson = {}
+for l in lines:
+  s = l.split(' ')
+  dwilson["XM."+s[0]] = s[5]
+f.close()
+
+f=open('depth_vpvs.txt', 'r')
+lines = f.readlines()
+ears = {}
+for l in lines:
+  s = l.split(' ')
+  ears[s[0]] = s[3]
+f.close()
+
+for sta in dwilson.keys():
+  print ears[sta], dwilson[sta], sta
+
+
+

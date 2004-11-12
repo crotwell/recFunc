3153276f9a2496a30d04496dca4c348b941bfca3
crotwell <crotwell@localhost>
2004-11-11 21:21:53 -0500

    [svn r11120] initial

---
+++ src/edu/sc/seis/receiverFunction/server/WeightTrial.java
@@ -0,0 +1,41 @@
+package edu.sc.seis.receiverFunction.server;
+
+import java.io.File;
+import java.util.ArrayList;
+import org.apache.log4j.BasicConfigurator;
+import edu.iris.Fissures.IfNetwork.StationId;
+import edu.sc.seis.fissuresUtil.exceptionHandler.GlobalExceptionHandler;
+import edu.sc.seis.receiverFunction.HKStack;
+import edu.sc.seis.receiverFunction.SumHKStack;
+
+/**
+ * @author crotwell Created on Oct 16, 2004
+ */
+public class WeightTrial {
+
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        try {
+            float minPercentMatch = 90f;
+            float smallestH = 20;
+            ArrayList list = JDBCHKStack.calcAndSave(args,
+                                                     minPercentMatch,
+                                                     false,
+                                                     0.7f,
+                                                     0.2f,
+                                                     0.1f);
+            HKStack[] stackArray = (HKStack[])list.toArray(new HKStack[0]);
+            SumHKStack sumStack = new SumHKStack(stackArray,
+                                                 stackArray[0].getChannel(),
+                                                 minPercentMatch,
+                                                 smallestH);
+            StackSummary.saveImage(sumStack,
+                                   stackArray[0].getChannel().my_site.my_station.get_id(),
+                                   new File("."),
+                                   minPercentMatch,
+                                   smallestH);
+        } catch(Exception e) {
+            GlobalExceptionHandler.handle(e);
+        }
+    }
+}

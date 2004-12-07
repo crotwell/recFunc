aa70bc032679038181dafd010ee40160cad8c697
crotwell <crotwell@localhost>
2004-12-07 14:35:10 -0500

    [svn r11377] works

--- src/edu/sc/seis/receiverFunction/compare/WilsonRistra.java
+++ src/edu/sc/seis/receiverFunction/compare/WilsonRistra.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 import edu.iris.Fissures.IfNetwork.StationId;
+import edu.iris.Fissures.network.StationIdUtil;
 
 
 /**
@@ -16,23 +17,19 @@ public class WilsonRistra implements StationCompare {
     public WilsonRistra() throws IOException {
         InputStream in = getClass().getClassLoader().getResourceAsStream("edu/sc/seis/receiverFunction/compare/WilsonRistra.prop");
         in = new BufferedInputStream(in);
-        Properties props = new Properties();
         props.load(in);
     }
     
-    /**
-     *
-     */
-    public float getVpVs(StationId stationId) {
-        // TODO Auto-generated method stub
-        return 0;
+    public StationResult getResult(StationId stationId) {
+        String prefix = StationIdUtil.toStringNoDates(stationId);
+        if (props.containsKey(prefix+"_H")) {
+            return new StationResult(stationId,
+                                     Float.parseFloat(props.getProperty(prefix+"_H")),
+                                     Float.parseFloat(props.getProperty(prefix+"_VpVs")));
+        } else {
+            return null;
+        }
     }
-
-    /**
-     *
-     */
-    public float getH(StationId stationId) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
+    
+    Properties props = new Properties();
 }

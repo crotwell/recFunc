8ce2dad3200c36f751b4caad0e44353236609690
crotwell <crotwell@localhost>
2005-02-21 16:45:19 -0500

    [svn r12165] coded, compiles, not tested

--- src/edu/sc/seis/receiverFunction/web/HKStackImageServlet.java
+++ src/edu/sc/seis/receiverFunction/web/HKStackImageServlet.java
@@ -1,16 +1,27 @@
 package edu.sc.seis.receiverFunction.web;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import edu.sc.seis.IfReceiverFunction.CachedResult;
 import edu.sc.seis.fissuresUtil.cache.CacheEvent;
 import edu.sc.seis.fissuresUtil.database.ConnMgr;
 import edu.sc.seis.fissuresUtil.database.event.JDBCEventAccess;
+import edu.sc.seis.fissuresUtil.database.network.JDBCChannel;
+import edu.sc.seis.receiverFunction.HKStack;
+import edu.sc.seis.receiverFunction.server.JDBCHKStack;
+import edu.sc.seis.receiverFunction.server.JDBCRecFunc;
+import edu.sc.seis.receiverFunction.server.JDBCSodConfig;
+import edu.sc.seis.receiverFunction.server.RecFuncCacheImpl;
 import edu.sc.seis.rev.StationLocator;
 import edu.sc.seis.rev.velocity.VelocityStation;
+import edu.sc.seis.sod.ConfigurationException;
 import edu.sc.seis.sod.database.waveform.JDBCEventChannelCookieJar;
 import edu.sc.seis.sod.database.waveform.JDBCEventChannelStatus;
 
@@ -21,23 +32,40 @@ public class HKStackImageServlet  extends HttpServlet {
 
     /**
      * @throws SQLException
+     * @throws ConfigurationException
+     * @throws Exception
+     * @throws 
+     * @throws ConfigurationException
      *
      */
-    public HKStackImageServlet() throws SQLException {
+    public HKStackImageServlet() throws SQLException, ConfigurationException, Exception {
         Connection conn = ConnMgr.createConnection();
         jdbcCookies = new JDBCEventChannelCookieJar(conn);
         jdbcEvent = new JDBCEventAccess(conn);
         jdbcECStatus = new JDBCEventChannelStatus(conn);
+        
+        JDBCChannel jdbcChannel  = new JDBCChannel(conn);
+        JDBCSodConfig jdbcSodConfig = new JDBCSodConfig(conn);
+        JDBCRecFunc jdbcRecFunc = new JDBCRecFunc(conn, jdbcEvent, jdbcChannel, jdbcSodConfig, RecFuncCacheImpl.getDataLoc());
+        hkStack = new JDBCHKStack(conn, jdbcEvent, jdbcChannel, jdbcSodConfig, jdbcRecFunc);
+
+        
     }
 
     public void doGet(HttpServletRequest req, HttpServletResponse res)
             throws IOException {
         try {
             logger.debug("doGet called");
+            if(req.getParameter("rf") == null) { throw new Exception("rf param not set"); }
+            int rf_id = new Integer(req.getParameter("rf")).intValue();
+            CachedResult result = hkStack.getJDBCRecFunc().get(rf_id);
+            HKStack stack = hkStack.get(rf_id);
+            BufferedImage image = stack.createStackImage();
+            
             res.setContentType("image/png");
-            if(req.getParameter("eq") == null) { throw new Exception("eq param not set"); }
-            CacheEvent eq = jdbcEvent.getEvent(new Integer(req.getParameter("eq")).intValue());
-            VelocityStation sta = staLoc.locate(req);
+            OutputStream out = res.getOutputStream();
+            ImageIO.write(image, "png", out);
+            out.close();
         } catch(Exception e) {
             e.printStackTrace();
             throw new RuntimeException(e);
@@ -51,6 +79,8 @@ public class HKStackImageServlet  extends HttpServlet {
     private JDBCEventChannelCookieJar jdbcCookies;
 
     private JDBCEventChannelStatus jdbcECStatus;
+    
+    private JDBCHKStack hkStack;
     
     private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(HKStackImageServlet.class);
     

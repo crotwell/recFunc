f21d6ffa2e7c963d97867dc0fccb67df1b4ac7fa
crotwell <crotwell@localhost>
2005-02-24 12:18:30 -0500

    [svn r12220] use new Rev populate method

--- src/edu/sc/seis/receiverFunction/web/Start.java
+++ src/edu/sc/seis/receiverFunction/web/Start.java
@@ -8,10 +8,8 @@ import edu.sc.seis.fissuresUtil.database.ConnMgr;
 import edu.sc.seis.fissuresUtil.simple.Initializer;
 import edu.sc.seis.rev.ServletFromSet;
 
-
 /**
- * @author crotwell
- * Created on Feb 10, 2005
+ * @author crotwell Created on Feb 10, 2005
  */
 public class Start {
 
@@ -27,38 +25,38 @@ public class Start {
         Properties props = Initializer.loadProperties(args);
         ConnMgr.setDB(ConnMgr.POSTGRES);
         ConnMgr.setURL(props.getProperty("sod.dburl"));
-        
-        String netHTML = "/networkList.html";
-        String staForNet = "/stationList.html";
-        String station = "/station.html";
-        String summaryHKStack = "/summaryHKStack.png";
-        String rfstationEventImage = "/hkstackimage.png";
-        
         Set servletStrings = new HashSet();
-        servletStrings.add(netHTML);
-        servletStrings.add(staForNet);
-        servletStrings.add(station);
-        servletStrings.add(rfstationEventImage);
-        servletStrings.add(summaryHKStack);
-        
         ServletHandler sh = new ServletFromSet(servletStrings);
-        sh.addServlet(netHTML,
-                      netHTML,
-                      "edu.sc.seis.receiverFunction.web.NetworkList");
-        sh.addServlet(staForNet,
-                      staForNet,
-                      "edu.sc.seis.receiverFunction.web.StationList");
-        sh.addServlet(station,
-                      station,
-                      "edu.sc.seis.receiverFunction.web.Station");
-        sh.addServlet(summaryHKStack,
-                      summaryHKStack,
-                      "edu.sc.seis.receiverFunction.web.SummaryHKStackImageServlet");
-//        sh.addServlet("rfStationEvent",
-//                      rfstationEvent,
-//                      "edu.sc.seis.receiverFunction.web.RFStationEvent");
-        sh.addServlet(rfstationEventImage,
-                      rfstationEventImage,
-                      "edu.sc.seis.receiverFunction.web.HKStackImageServlet");
-        edu.sc.seis.rev.Start.runREV(args, sh);}
+        edu.sc.seis.rev.Start.populateJetty("/networkList.html",
+                                            "/networkList.html",
+                                            "edu.sc.seis.receiverFunction.web.NetworkList",
+                                            servletStrings,
+                                            sh);
+        edu.sc.seis.rev.Start.populateJetty("/stationList.html",
+                                            "/stationList.html",
+                                            "edu.sc.seis.receiverFunction.web.StationList",
+                                            servletStrings,
+                                            sh);
+        edu.sc.seis.rev.Start.populateJetty("/station.html",
+                                            "/station.html",
+                                            "edu.sc.seis.receiverFunction.web.Station",
+                                            servletStrings,
+                                            sh);
+        edu.sc.seis.rev.Start.populateJetty("/summaryHKStack.png",
+                                            "/summaryHKStack.png",
+                                            "edu.sc.seis.receiverFunction.web.SummaryHKStackImageServlet",
+                                            servletStrings,
+                                            sh);
+        edu.sc.seis.rev.Start.populateJetty("/stationEvent.html",
+                                            "/stationEvent.html",
+                                            "edu.sc.seis.receiverFunction.web.RFStationEvent",
+                                            servletStrings,
+                                            sh);
+        edu.sc.seis.rev.Start.populateJetty("/hkstackimage.png",
+                                            "/hkstackimage.png",
+                                            "edu.sc.seis.receiverFunction.web.HKStackImageServlet",
+                                            servletStrings,
+                                            sh);
+        edu.sc.seis.rev.Start.runREV(args, sh);
+    }
 }

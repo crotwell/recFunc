2dc257d4a11514499103e71a34b097eeea9477ed
crotwell <crotwell@localhost>
2005-09-05 14:30:04 -0400

    [svn r14695] make b&w

--- paper/ristra/xyplot.sh
+++ paper/ristra/xyplot.sh
@@ -6,18 +6,9 @@ REGION=-111/-102.5/20/70
 /bin/rm -f ${NAME}H.ps
 
 touch ${NAME}H.ps
-perl -nae 'print "$F[4] $F[5] $F[6] $F[6]\n"' ${NAME}.txt | psxy -K -P -H2 -JX6/-8 -R${REGION} -B2/2 -Sc.1 -G255/0/0 -Ey >> ${NAME}H.ps
-#perl -nae 'if($F[6]>5) {print "$F[4] $F[5] $F[6] $F[6]\n"}' ${NAME}.txt | psxy -K -O -JX -R -B2/2 -Sc.1 -G0/0/255 -Ey >> ${NAME}H.ps
+perl -nae 'print "$F[4] $F[5] $F[6] $F[6]\n"' ${NAME}.txt | psxy -K -P -H2 -JX6i/-8i -R${REGION} -B2:"Longitude":/5:"Thickness": -Sc.1i -G155 -Ey >> ${NAME}H.ps
+perl -nae 'print "$F[4] $F[13]\n"' ${NAME}.txt | psxy -O -K -H2 -JX -R -Ss.1i -G0 >> ${NAME}H.ps
 
-perl -nae 'print "$F[4] $F[5] 9 0 1 ML .$F[1]\n"' ${NAME}.txt | pstext  -O -K  -H2 -JX -R >> ${NAME}H.ps
+perl -nae 'print "$F[4] 30 9 90 1 ML $F[1]\n"' ${NAME}.txt | pstext  -O -H2 -JX -R -Y1.2i >> ${NAME}H.ps
 
-perl -nae 'print "$F[4] $F[13]\n"' ${NAME}.txt | psxy -O -H2 -JX -R -Ss.1 -G0/255/0 >> ${NAME}H.ps
 
-FILENAME=bw${NAME}H.ps
-/bin/rm -f $FILENAME
-touch $FILENAME
-perl -nae 'print "$F[4] $F[5] $F[6] $F[6]\n"' ${NAME}.txt | psxy -K  -H2 -JX8/-6 -R${REGION} -B2/2 -Sc.1 -Ey >> $FILENAME
-
-perl -nae '$x=$F[4]+.08 ; print "$x $F[5] 9 0 1 ML $F[1]\n"' ${NAME}.txt | pstext  -O -K  -H2 -JX -R >> $FILENAME
-
-perl -nae 'print "$F[4] $F[13]\n"' ${NAME}.txt | psxy -O -H2 -JX -R -Ss.1 >> $FILENAME

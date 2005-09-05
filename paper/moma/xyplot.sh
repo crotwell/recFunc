ac488ac284949aa8f4b63c498abc1c27e649a972
crotwell <crotwell@localhost>
2005-09-05 13:53:58 -0400

    [svn r14694] B&W, add labels

--- paper/moma/xyplot.sh
+++ paper/moma/xyplot.sh
@@ -6,9 +6,9 @@ REGION=-93/-70.5/20/70
 /bin/rm -f ${NAME}H.ps
 
 touch ${NAME}H.ps
-perl -nae 'print "$F[4] $F[5] $F[6] $F[6]\n"' ${NAME}.txt | psxy -K -P -H2 -JX6/-8 -R${REGION} -B2/2 -Sc.1 -G255/0/0 -Ey >> ${NAME}H.ps
+perl -nae 'print "$F[4] $F[5] $F[6] $F[6]\n"' ${NAME}.txt | psxy -K -P -H2 -JX6i/-8i -R${REGION} -B2:"Longitude":/5:"Thickness km": -Sc.1i -G155 -Ey >> ${NAME}H.ps
 perl -nae 'print "$F[4] $F[5] 9 0 1 ML .$F[1]\n"' ${NAME}.txt | pstext  -O -K -H2 -JX -R >> ${NAME}H.ps
 
-perl -nae 'print "$F[4] $F[13]\n"' ${NAME}.txt | psxy -O -H2 -JX -R -Ss.1 -G0/255/0 >> ${NAME}H.ps
+perl -nae 'print "$F[4] $F[13]\n"' ${NAME}.txt | psxy -O -H2 -JX -R -Ss.1i -G0 >> ${NAME}H.ps
 
 

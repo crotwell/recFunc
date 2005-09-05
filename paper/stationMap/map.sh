681a8eb28fa3c3e2cc4ac136bef3d13c1a78e1e0
crotwell <crotwell@localhost>
2005-09-05 19:44:46 -0400

    [svn r14696] map smaller and clean up labels

--- paper/stationMap/map.sh
+++ paper/stationMap/map.sh
@@ -6,8 +6,8 @@ OUT=stationMap.ps
 
 touch $OUT
 
-psbasemap  -K -JN0/9 -R-180/180/-90/90  -Ba30g30/a30g15 >> $OUT
-pscoast  -K -O -JN -R  -S210  -Di -A10000 -Ba30g30/a30g15 >> $OUT
-perl -nae 'print "$F[3] $F[2]\n"' ears_latlon.txt | psxy -O -JN -R -St.07 -G5 >> $OUT
+psbasemap  -K -P -JN0/5i -R-180/180/-90/90  -Bg30/g15 >> $OUT
+pscoast  -K -O -JN -R  -S210  -Di -A10000 -Bg30/g15 >> $OUT
+perl -nae 'print "$F[3] $F[2]\n"' ears_latlon.txt | psxy -O -JN -R -St.07i -G5 >> $OUT
 
 

51c393528d19afdc822f9bc5b1cf76f1187c44ef
crotwell <crotwell@localhost>
2005-09-06 21:48:31 -0400

    [svn r14705] partial

--- paper/westUS/westUS.sh
+++ paper/westUS/westUS.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-#curl -o scal.txt 'http://www.seis.sc.edu/ears_tmp/stationLatLonBox.html?minLat=30&maxLat=33&minLon=-113&maxLon=-119'
 
 FILENAME=westUS.ps
+rm -f $FILENAME
 touch $FILENAME
-pscoast -JM6 -R-120/-110/30/45 -Di -B5/5 -Na -W >> $FILENAME
+pscoast -JM6i -R-130/-100/30/50 -Di -B5/5 -Na -W >> $FILENAME
+
+
+#curl -o scal.txt 'http://www.seis.sc.edu/ears_tmp/stationLatLonBox.txt?minLat=30&maxLat=35&minLon=-130&maxLon=-100'
+grep -v '\.\.\.' scal.txt | cleantxt.pl | perl -nae 'print "$F[3] $F[4]\n"' | psxy -JX6i/-6i  -R-130/-100/0/70 -Sc.1i -H1 -B5/5 > scal.ps
 
 

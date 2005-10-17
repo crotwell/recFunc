645e8b2f600c5f65ed39ec0c9ba0af5635a05b86
crotwell <crotwell@localhost>
2005-10-17 10:24:52 -0400

    [svn r14912] initial

---
+++ src/edu/sc/seis/receiverFunction/synth/SimpleSynthReceiverFunction.java
@@ -0,0 +1,10 @@
+package edu.sc.seis.receiverFunction.ray3d;
+
+
+public class SimpleSynthReceiverFunction {
+
+    public SimpleSynthReceiverFunction() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+}

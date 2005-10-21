75c8759392fc34d32ffc97f220b662120dfe98a3
crotwell <crotwell@localhost>
2005-10-21 13:29:07 -0400

    [svn r14954] initial

---
+++ src/edu/sc/seis/receiverFunction/StackComplexity.java
@@ -0,0 +1,88 @@
+package edu.sc.seis.receiverFunction;
+
+import edu.iris.Fissures.FissuresException;
+import edu.iris.Fissures.Sampling;
+import edu.iris.Fissures.model.QuantityImpl;
+import edu.iris.Fissures.seismogramDC.LocalSeismogramImpl;
+import edu.sc.seis.fissuresUtil.bag.TauPUtil;
+import edu.sc.seis.fissuresUtil.chooser.ClockUtil;
+import edu.sc.seis.receiverFunction.compare.StationResult;
+import edu.sc.seis.receiverFunction.synth.SimpleSynthReceiverFunction;
+
+/**
+ * @author crotwell Created on Oct 21, 2005
+ */
+public class StackComplexity {
+
+    public StackComplexity(SumHKStack stack, int num_points) {
+        this.stack = stack;
+        this.samp = stack.getChannel().sampling_info;
+        this.num_points = num_points;
+    }
+
+    public HKStack getResidual(StationResult staResult)
+            throws FissuresException {
+        SimpleSynthReceiverFunction synth = new SimpleSynthReceiverFunction(staResult,
+                                                                            samp,
+                                                                            num_points);
+        LocalSeismogramImpl synthRF = synth.calculate(sphRayParam,
+                                                      ClockUtil.now()
+                                                              .getFissuresTime(),
+                                                      RecFunc.getDefaultShift(),
+                                                      stack.getChannel()
+                                                              .get_id());
+        HKStack synthStack = new HKStack(stack.getSum().getAlpha(),
+                                         sphRayParam,
+                                         100,
+                                         stack.getSum().minH,
+                                         stack.getSum().stepH,
+                                         stack.getSum().numH,
+                                         stack.getSum().minK,
+                                         stack.getSum().stepK,
+                                         stack.getSum().numK,
+                                         1 / 3f,
+                                         1 / 3f,
+                                         1 / 3f,
+                                         synthRF,
+                                         stack.getChannel(),
+                                         RecFunc.getDefaultShift());
+        float[][] data = stack.getSum().getStack();
+        float[][] synthData = synthStack.getStack();
+        // scale synth data by max of data so best HK -> 0
+        // hopefully this subtracts the bulk of the "mountain" around the max
+        QuantityImpl smallestH = HKStack.getBestSmallestH(stack.getChannel().my_site.my_station);
+        float scale = stack.getSum().getMaxValue(smallestH)
+                / synthStack.getMaxValue(smallestH);
+        float[][] diff = new float[stack.getSum().numH][stack.getSum().numK];
+        for(int i = 0; i < diff.length; i++) {
+            for(int jj = 0; jj < diff[0].length; jj++) {
+                diff[i][jj] = data[i][jj] - synthData[i][jj] * scale;
+            }
+        }
+        return new HKStack(stack.getSum().getAlpha(),
+                           sphRayParam,
+                           -1,
+                           stack.getSum().minH,
+                           stack.getSum().stepH,
+                           stack.getSum().numH,
+                           stack.getSum().minK,
+                           stack.getSum().stepK,
+                           stack.getSum().numK,
+                           1 / 3f,
+                           1 / 3f,
+                           1 / 3f,
+                           diff,
+                           stack.getChannel());
+    }
+
+    static final float sphRayParam = 6.877f * (float)Math.PI / 180; // s/deg for
+
+    // P for 60
+    // deg
+    // distancce
+    SumHKStack stack;
+
+    Sampling samp;
+
+    int num_points;
+}
---
+++ src/edu/sc/seis/receiverFunction/web/ComplexityResidualImage.java
@@ -0,0 +1,33 @@
+package edu.sc.seis.receiverFunction.web;
+
+import java.sql.SQLException;
+import javax.servlet.http.HttpServletRequest;
+import edu.sc.seis.receiverFunction.HKStack;
+import edu.sc.seis.receiverFunction.StackComplexity;
+import edu.sc.seis.receiverFunction.SumHKStack;
+import edu.sc.seis.receiverFunction.compare.StationResult;
+import edu.sc.seis.sod.ConfigurationException;
+import edu.sc.seis.sod.velocity.network.VelocityNetwork;
+
+
+/**
+ * @author crotwell
+ * Created on Oct 21, 2005
+ */
+public class ComplexityResidualImage extends SummaryHKStackImageServlet {
+
+
+    public ComplexityResidualImage() throws SQLException, ConfigurationException, Exception {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public SumHKStack getSumStack(HttpServletRequest req,
+                                  VelocityNetwork net,
+                                  String staCode) throws Exception {
+        SumHKStack stack = super.getSumStack(req, net, staCode);
+        StackComplexity complexity = new StackComplexity(stack, 256);
+        StationResult model = new StationResult(net.get_id(), staCode, stack.getSum().getMaxValueH(stack.getSmallestH()), stack.getSum().getMaxValueK(stack.getSmallestH()), stack.getSum().getAlpha(), null);
+        return new SumHKStack(stack.getMinPercentMatch(), stack.getSmallestH(), complexity.getResidual(model), -1, -1, stack.getNumEQ());
+    }
+}

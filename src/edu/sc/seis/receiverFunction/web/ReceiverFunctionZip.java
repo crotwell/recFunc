d9119db67865e46b4313510dc9cefff5f4111b84
crotwell <crotwell@localhost>
2005-10-24 14:59:59 -0400

    [svn r14987] add transverse to zip

--- src/edu/sc/seis/receiverFunction/web/ReceiverFunctionZip.java
+++ src/edu/sc/seis/receiverFunction/web/ReceiverFunctionZip.java
@@ -25,6 +25,7 @@ import edu.sc.seis.receiverFunction.server.JDBCRecFunc;
 import edu.sc.seis.receiverFunction.server.JDBCSodConfig;
 import edu.sc.seis.receiverFunction.server.RecFuncCacheImpl;
 import edu.sc.seis.rev.RevUtil;
+import edu.sc.seis.rev.Revlet;
 import edu.sc.seis.seisFile.sac.SacTimeSeries;
 import edu.sc.seis.sod.ConfigurationException;
 import edu.sc.seis.sod.velocity.event.VelocityEvent;
@@ -72,39 +73,49 @@ public class ReceiverFunctionZip extends HttpServlet {
                 VelocityEvent event = new VelocityEvent(new CacheEvent(result[i].event_attr,
                                                                        result[i].prefOrigin));
                 VelocityStation sta = new VelocityStation(result[i].channels[2].my_site.my_station);
-                String entryName = TOP_ZIP_DIR + sta.getNetCode() + "."
-                + sta.getCode() + "/" + event.getFilizedTime() + ".itr";
-                String origEntryName = entryName;
-                int j=2;
-                while(knownEntries.contains(entryName)) {
-                    entryName = origEntryName+"."+j;
-                    j++;
+                // 0 for radial, 1 for transverse
+                for(int rfType = 0; rfType < 2; rfType++) {
+                    String entryName = TOP_ZIP_DIR + sta.getNetCode() + "."
+                            + sta.getCode() + "/"
+                            + event.getTime("yyyy_DDD_HH_mm_ss") + (rfType == 0 ? ".itr" : "itt");
+                    String origEntryName = entryName;
+                    int j = 2;
+                    while(knownEntries.contains(entryName)) {
+                        entryName = origEntryName + "." + j;
+                        j++;
+                    }
+                    knownEntries.add(entryName);
+                    ZipEntry entry = new ZipEntry(entryName);
+                    System.out.println("Start new ZipEntry: " + entry);
+                    zip.putNextEntry(entry);
+                    LocalSeismogramImpl rfSeis = (LocalSeismogramImpl) (rfType == 0 ? result[i].radial : result[i].tansverse);
+                    SacTimeSeries sac = FissuresToSac.getSAC(rfSeis,
+                                                             result[i].channels[2],
+                                                             result[i].prefOrigin);
+                    sac.writeHeader(dos);
+                    sac.writeData(dos);
+                    dos.flush();
+                    zip.closeEntry();
                 }
-                knownEntries.add(entryName);
-                ZipEntry entry = new ZipEntry(entryName);
-                System.out.println("Start new ZipEntry: "+entry);
-                zip.putNextEntry(entry);
-                SacTimeSeries sac = FissuresToSac.getSAC((LocalSeismogramImpl)result[i].radial,
-                                                         result[i].channels[2],
-                                                         result[i].prefOrigin);
-                sac.writeHeader(dos);
-                sac.writeData(dos);
-                dos.flush();
-                zip.closeEntry();
             }
-            if (result.length == 0) {
+            if(result.length == 0) {
                 // add at least one file so zip doesn't complain
-                ZipEntry entry = new ZipEntry(TOP_ZIP_DIR+"no_data.txt");
+                ZipEntry entry = new ZipEntry(TOP_ZIP_DIR + "no_data.txt");
                 zip.putNextEntry(entry);
-                dos.writeChars("Sorry, there was no data for network "+netDbId+" station: "+staCode+"\n");
+                dos.writeChars("Sorry, there was no data for network "
+                        + netDbId + " station: " + staCode + "\n");
                 dos.flush();
                 zip.closeEntry();
             }
             zip.close();
         } catch(Exception e) {
-            GlobalExceptionHandler.handle(e);
+            Revlet.sendToGlobalExceptionHandler(req, e);
             throw new ServletException(e);
         }
+    }
+    
+    protected void writeEntry() {
+        
     }
     
     private static final String TOP_ZIP_DIR = "Ears/";

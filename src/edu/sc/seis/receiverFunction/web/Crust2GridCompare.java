21f8c5dc2af150d95adb0383777ba67525f1caa0
crotwell <crotwell@localhost>
2006-03-01 22:44:43 -0500

    [svn r16388] reindent

--- src/edu/sc/seis/receiverFunction/web/Crust2GridCompare.java
+++ src/edu/sc/seis/receiverFunction/web/Crust2GridCompare.java
@@ -20,89 +20,92 @@ import edu.sc.seis.rev.RevUtil;
 import edu.sc.seis.rev.Revlet;
 import edu.sc.seis.rev.RevletContext;
 
-
 public class Crust2GridCompare extends Revlet {
-    
-    public Crust2GridCompare() throws Exception {
-        Connection conn = ConnMgr.createConnection();
-        JDBCRecFunc jdbcRecFunc = new JDBCRecFunc(conn,
-                                      Start.getDataLoc());
-        JDBCHKStack jdbcHKStack = new JDBCHKStack(jdbcRecFunc);
-        jdbcSummaryHKStack = new JDBCSummaryHKStack(jdbcHKStack);
-    }
 
-    public RevletContext getContext(HttpServletRequest req, HttpServletResponse res) throws Exception {
-        int minPerCell = RevUtil.getInt("minPerCell", req, 5);
-        String path = req.getServletPath();
-        String vmFile;
-        if(path.endsWith(".txt")) {
-            vmFile = "crust2GridCompareTxt.vm";
-        } else if(path.endsWith(".html")) {
-            vmFile = "crust2GridCompare.vm";
-        } else {
-            vmFile = "crust2GridCompare.vm";
-        }
-        RevletContext context = new RevletContext(vmFile,
-                                                  Start.getDefaultContext());
-        ArrayList summaries = jdbcSummaryHKStack.getAllWithoutData();
-        Iterator it = summaries.iterator();
-        HashMap sumByGrid = new HashMap();
-        while(it.hasNext()) {
-            SumHKStack sum = (SumHKStack)it.next();
-            Location loc = sum.getChannel().my_site.my_station.my_location;
-            int[] ll = Crust2.getClosestLatLon(loc.longitude, loc.latitude);
-            String llStr = ll[0]+","+ll[1];
-            List gridList;
-            if (! sumByGrid.containsKey(llStr)) {
-            	gridList = new ArrayList();
-                sumByGrid.put(llStr, gridList);
-            } else {
-            	gridList = (List)sumByGrid.get(llStr);
-            }
-            gridList.add(sum);
-        }
-        it = sumByGrid.keySet().iterator();
-        ArrayList output = new ArrayList();
-        context.put("gridLists", output);
-        while(it.hasNext()) {
-            String key = (String)it.next();
-            List gridList = (List)sumByGrid.get(key);
-            if (gridList.size() < minPerCell) {
-                it.remove();
-                continue;
-            }
-            float hAvg = 0;
-            float kAvg = 0;
-            Iterator listIt = gridList.iterator();
-            SumHKStack sum = null;
-            while(listIt.hasNext()) {
-                sum = (SumHKStack)listIt.next();
-                hAvg += sum.getSum().getMaxValueH().getValue(UnitImpl.KILOMETER);
-                kAvg += sum.getSum().getMaxValueK();
-            }
-            hAvg /= gridList.size();
-            kAvg /= gridList.size();
-            Location loc = sum.getChannel().my_site.my_station.my_location;
-            int[] latLon = Crust2.getClosestLatLon( loc);
-            output.add(new Crust2GridCompareResult(hAvg, kAvg, latLon[0], latLon[1], gridList, crust2.getByCode(crust2.getClosestCode(loc))));
-        }
-        return context;
-    }
+	public Crust2GridCompare() throws Exception {
+		Connection conn = ConnMgr.createConnection();
+		JDBCRecFunc jdbcRecFunc = new JDBCRecFunc(conn, Start.getDataLoc());
+		JDBCHKStack jdbcHKStack = new JDBCHKStack(jdbcRecFunc);
+		jdbcSummaryHKStack = new JDBCSummaryHKStack(jdbcHKStack);
+	}
 
-    protected void setContentType(HttpServletRequest req,
-                                  HttpServletResponse response) {
-        String path = req.getServletPath();
-        if(path.endsWith(".txt")) {
-            response.setContentType("text/plain");
-        } else if(path.endsWith(".xml")) {
-            response.setContentType("text/xml");
-        } else if(path.endsWith(".html")) {
-            response.setContentType("text/html");
-        } else {
-            throw new RuntimeException("Unknown URL: " + req.getRequestURI());
-        }
-    }
+	public RevletContext getContext(HttpServletRequest req,
+			HttpServletResponse res) throws Exception {
+		int minPerCell = RevUtil.getInt("minPerCell", req, 5);
+		String path = req.getServletPath();
+		String vmFile;
+		if (path.endsWith(".txt")) {
+			vmFile = "crust2GridCompareTxt.vm";
+		} else if (path.endsWith(".html")) {
+			vmFile = "crust2GridCompare.vm";
+		} else {
+			vmFile = "crust2GridCompare.vm";
+		}
+		RevletContext context = new RevletContext(vmFile, Start
+				.getDefaultContext());
+		ArrayList summaries = jdbcSummaryHKStack.getAllWithoutData();
+		Iterator it = summaries.iterator();
+		HashMap sumByGrid = new HashMap();
+		while (it.hasNext()) {
+			SumHKStack sum = (SumHKStack) it.next();
+			Location loc = sum.getChannel().my_site.my_station.my_location;
+			int[] ll = Crust2.getClosestLatLon(loc.longitude, loc.latitude);
+			String llStr = ll[0] + "," + ll[1];
+			List gridList;
+			if (!sumByGrid.containsKey(llStr)) {
+				gridList = new ArrayList();
+				sumByGrid.put(llStr, gridList);
+			} else {
+				gridList = (List) sumByGrid.get(llStr);
+			}
+			gridList.add(sum);
+		}
+		it = sumByGrid.keySet().iterator();
+		ArrayList output = new ArrayList();
+		context.put("gridLists", output);
+		while (it.hasNext()) {
+			String key = (String) it.next();
+			List gridList = (List) sumByGrid.get(key);
+			if (gridList.size() < minPerCell) {
+				it.remove();
+				continue;
+			}
+			float hAvg = 0;
+			float kAvg = 0;
+			Iterator listIt = gridList.iterator();
+			SumHKStack sum = null;
+			while (listIt.hasNext()) {
+				sum = (SumHKStack) listIt.next();
+				hAvg += sum.getSum().getMaxValueH()
+						.getValue(UnitImpl.KILOMETER);
+				kAvg += sum.getSum().getMaxValueK();
+			}
+			hAvg /= gridList.size();
+			kAvg /= gridList.size();
+			Location loc = sum.getChannel().my_site.my_station.my_location;
+			int[] latLon = Crust2.getClosestLatLon(loc);
+			output.add(new Crust2GridCompareResult(hAvg, kAvg, latLon[0],
+					latLon[1], gridList, crust2.getByCode(crust2
+							.getClosestCode(loc))));
+		}
+		return context;
+	}
 
-    Crust2 crust2 = new Crust2();
-    JDBCSummaryHKStack jdbcSummaryHKStack;
+	protected void setContentType(HttpServletRequest req,
+			HttpServletResponse response) {
+		String path = req.getServletPath();
+		if (path.endsWith(".txt")) {
+			response.setContentType("text/plain");
+		} else if (path.endsWith(".xml")) {
+			response.setContentType("text/xml");
+		} else if (path.endsWith(".html")) {
+			response.setContentType("text/html");
+		} else {
+			throw new RuntimeException("Unknown URL: " + req.getRequestURI());
+		}
+	}
+
+	Crust2 crust2 = new Crust2();
+
+	JDBCSummaryHKStack jdbcSummaryHKStack;
 }

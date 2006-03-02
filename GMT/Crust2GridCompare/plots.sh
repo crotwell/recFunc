2914e5a5a3f6573a3c4b3f68593f7e124268c88d
crotwell <crotwell@localhost>
2006-03-01 20:59:11 -0500

    [svn r16380] initial

---
+++ GMT/Crust2GridCompare/plots.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# PE 6.35 3.61 1.76 50 38.75 1.75 -113.0 47.0
+
+OUT=map
+CRUST2_DATA=crust2GridCompare_2.txt
+rm -f ${OUT}.ps 
+pscoast -K -JM9i -R-180/180/-80/80 -B30 -A1000 -Di -W >> ${OUT}.ps
+
+perl -nae '$d=$F[4]-$F[5];$a=$F[7]-1;$b=$F[8]-1;print "> -Z$d\n";print "$a $b\n";$a++;print "$a $b\n";$b++;print "$a $b\n";$a--;print "$a $b\n"' \
+   $CRUST2_DATA | psxy -O -JM -R -M -L  -Credblue.cpt >> ${OUT}.ps
+ps2pdf ${OUT}.ps
+rm -f ${OUT}.ps
+open ${OUT}.pdf

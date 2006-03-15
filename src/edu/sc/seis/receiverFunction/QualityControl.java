a3494e544835757791aa65b46f5787a6a4876d4e
crotwell <crotwell@localhost>
2006-03-15 16:52:49 -0500

    [svn r16447] work on all for a network

--- src/edu/sc/seis/receiverFunction/QualityControl.java
+++ src/edu/sc/seis/receiverFunction/QualityControl.java
@@ -3,10 +3,14 @@ package edu.sc.seis.receiverFunction;
 import java.io.FileWriter;
 import java.sql.Connection;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 import edu.iris.Fissures.FissuresException;
 import edu.iris.Fissures.IfNetwork.NetworkId;
 import edu.iris.Fissures.IfNetwork.Station;
+import edu.iris.Fissures.IfNetwork.StationId;
 import edu.iris.Fissures.model.TimeInterval;
 import edu.iris.Fissures.model.UnitImpl;
 import edu.iris.Fissures.network.NetworkIdUtil;
@@ -63,8 +67,10 @@ public class QualityControl {
                           radial);
         Statistics radialStats = new Statistics(radial);
         Statistics radialPStats = new Statistics(radialP);
-        return (float)(radialPStats.max() / radialStats.maxDeviation());
+        return (float)((radialPStats.max() - radialStats.mean()) / radialStats.maxDeviation());
     }
+
+    public void calcForStation(int netDbId, String staCode) {}
 
     public static void main(String[] args) {
         if(args.length == 0) {
@@ -88,42 +94,63 @@ public class QualityControl {
                     staArg = args[i + 1];
                 }
             }
-            if(staArg.equals("")) {
-                System.out.println("Must give a station with -sta");
-                return;
-            } else {
-                logger.info("calc for station: " + netArg + "." + staArg);
-                JDBCStation jdbcStation = jdbcRecFunc.getJDBCChannel()
-                        .getStationTable();
-                NetworkId[] nets = jdbcStation.getNetTable().getByCode(netArg);
-                int sta_dbid = -1;
-                for(int i = 0; i < nets.length; i++) {
-                    try {
-                        int[] tmp = jdbcStation.getDBIds(nets[i], staArg);
-                        if(tmp.length > 0) {
-                            sta_dbid = tmp[0];
-                            Station station = jdbcStation.get(sta_dbid);
-                            int netDbId = jdbcStation.getNetTable()
-                                    .getDbId(station.my_network.get_id());
-                            CachedResult[] results = jdbcRecFunc.getByPercent(netDbId,
-                                                                              staArg,
-                                                                              2.5f,
-                                                                              80f);
-                            for(int j = 0; j < results.length; j++) {
-                                System.out.println(StationIdUtil.toStringFormatDates(station)
+            logger.info("calc for station: " + netArg + "." + staArg);
+            JDBCStation jdbcStation = jdbcRecFunc.getJDBCChannel()
+                    .getStationTable();
+            NetworkId[] nets = jdbcStation.getNetTable().getByCode(netArg);
+            System.out.println("calc for "+nets.length+" nets");
+            for(int i = 0; i < nets.length; i++) {
+                try {
+                    String[] staCodes;
+                    if(staArg == "") {
+                        StationId[] staIds = jdbcStation.getAllStationIds(nets[i]);
+                        Set allCodes = new HashSet();
+                        for(int j = 0; j < staIds.length; j++) {
+                            allCodes.add(staIds[j]);
+                        }
+                        staCodes = (String[])allCodes.toArray(new String[0]);
+                    } else {
+                        staCodes = new String[] {staArg};
+                    }
+                    for(int j = 0; j < staCodes.length; j++) {
+                        int[] dbids = jdbcStation.getDBIds(nets[i], staCodes[j]);
+                        Station station = jdbcStation.get(dbids[0]);
+                        int numGood = 0;
+                        int netDbId = jdbcStation.getNetTable()
+                                .getDbId(station.my_network.get_id());
+                        CachedResult[] results = jdbcRecFunc.getByPercent(netDbId,
+                                                                          staCodes[j],
+                                                                          2.5f,
+                                                                          80f);
+                        for(int r = 0; r < results.length; r++) {
+                            float tToR = control.transverseToRadial(results[r]);
+                            float pAmp = control.radialPAmp(results[r]);
+                            if(tToR > .5 || pAmp < .8) {
+                                System.out.println(decFormat.format(results[r].radialMatch)
+                                        + " "
+                                        + StationIdUtil.toStringFormatDates(station)
                                         + " origin="
-                                        + results[j].prefOrigin.origin_time.date_time
+                                        + results[r].prefOrigin.origin_time.date_time
                                         + "  T to R="
-                                        + decFormat.format(control.transverseToRadial(results[j]))
-                                        + "  P amp="
-                                        + decFormat.format(control.radialPAmp(results[j])));
+                                        + decFormat.format(tToR)
+                                        + "  P amp=" + decFormat.format(pAmp));
+                            } else {
+                                numGood++;
                             }
                         }
-                    } catch(NotFound e) {
-                        System.out.println("NotFound for :"
-                                + NetworkIdUtil.toStringNoDates(nets[i]));
-                        // go to next network
+                        System.out.println(StationIdUtil.toStringNoDates(station)
+                                + " Num good = "
+                                + numGood
+                                + " out of "
+                                + results.length
+                                + "  => "
+                                + decFormat.format(numGood * 100.0
+                                        / results.length));
                     }
+                } catch(NotFound e) {
+                    System.out.println("NotFound for :"
+                            + NetworkIdUtil.toStringNoDates(nets[i]));
+                    // go to next network
                 }
             }
         } catch(Exception e) {

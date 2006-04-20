ddd7bd7648891c97c20ced026ddcd466e2c53eee
crotwell <crotwell@localhost>
2006-04-20 16:47:05 -0400

    [svn r16945] redo test for more than one point

--- test/unit/edu/sc/seis/receiverFunction/synth/SimpleSynthReveicerFunctionTest.java
+++ test/unit/edu/sc/seis/receiverFunction/synth/SimpleSynthReveicerFunctionTest.java
@@ -18,93 +18,116 @@ public class SimpleSynthReveicerFunctionTest extends TestCase {
 
     public SimpleSynthReveicerFunctionTest(String arg0) {
         super(arg0);
-        simple = new SimpleSynthReceiverFunction(new StationResult(MockNetworkId.createNetworkID(),
-                                                                   "test",
-                                                                   new QuantityImpl(30, UnitImpl.KILOMETER),
-                                                                   (float)(vpCrust / vsCrust),
-                                                                   new QuantityImpl(vpCrust,
-                                                                                    UnitImpl.KILOMETER_PER_SECOND),
-                                                                   null),
-                                                 new SamplingImpl(1, new TimeInterval(1, UnitImpl.SECOND)),
-                                                 4096,
-                                                 rhoCrust,
-                                                 vpMantle,
-                                                 vsMantle,
-                                                 rhoMantle);
+    }
+
+    public void testJordiA() {
+        double vpCrust = 6.0;
+        double vsCrust = 3.5;
+        double rhoCrust = 3.0;
+        double vpMantle = 7;
+        double vsMantle = 4.2;
+        double rhoMantle = 4.0;
+        float flatRP = 0.03f;
+        double[] a = new double[] {0.213547871,
+                                   0.0486433432,
+                                   0.0977657735,
+                                   -0.0919868574};
+        double rsr0 = 4.734231;
+        double zsz0 = -0.124569595;
+        double rpz0 = 0.213547871;
+        SimpleSynthReceiverFunction simple = new SimpleSynthReceiverFunction(new StationResult(MockNetworkId.createNetworkID(),
+                                                                                               "test",
+                                                                                               new QuantityImpl(30,
+                                                                                                                UnitImpl.KILOMETER),
+                                                                                               (float)(vpCrust / vsCrust),
+                                                                                               new QuantityImpl(vpCrust,
+                                                                                                                UnitImpl.KILOMETER_PER_SECOND),
+                                                                                               null),
+                                                                             new SamplingImpl(1,
+                                                                                              new TimeInterval(1,
+                                                                                                               UnitImpl.SECOND)),
+                                                                             4096,
+                                                                             rhoCrust,
+                                                                             vpMantle,
+                                                                             vsMantle,
+                                                                             rhoMantle);
         simple.calculate(flatRP,
                          ClockUtil.now().getFissuresTime(),
                          new TimeInterval(0, UnitImpl.SECOND),
                          MockChannel.createChannel().get_id(),
                          5);
-    }
-    
-    public void testJordiBasicTerms() {
         assertEquals("rsr0", rsr0, simple.rsr0, 0.00001);
         assertEquals("zsz0", zsz0, simple.zsz0, 0.00001);
         assertEquals("rpz0", rpz0, simple.rpz0, 0.00001);
-    }
-    
-    public void testJordiRefTransPP0() {
-        assertEquals("PP0", -0.94872421, simple.downgoingRFCoeff.getFreePtoPRefl(flatRP), 0.00001);
-    }
-    public void testJordiRefTransPS0() {
-        assertEquals("PS0", 0.411624223, simple.downgoingRFCoeff.getFreePtoSVRefl(flatRP), 0.00001);
-    }
-    public void testJordiRefTransSP0() {
-        assertEquals("SP0", 0.242751762, simple.downgoingRFCoeff.getFreeSVtoPRefl(flatRP), 0.00001);
-    }
-    
-    public void testJordiRefTransPS2() {
-        assertEquals("PS2", -0.0896627381, simple.downgoingRFCoeff.getPtoSVRefl(flatRP), 0.00001);
-    }
-    
-    public void testJordiRefTransPP2() {
-        assertEquals("PP2", 0.205925092, simple.downgoingRFCoeff.getPtoPRefl(flatRP), 0.00001);
-    }
-    
-    public void testJordiRefTransSP2() {
-        assertEquals("PS2", -0.0528778173, simple.downgoingRFCoeff.getSVtoPRefl(flatRP), 0.00001);
-    }
-    
-    public void testJordiRefTransSS2() {
-        assertEquals("PP2", -0.217589691, simple.downgoingRFCoeff.getSVtoSVRefl(flatRP), 0.00001);
-    }
-
-    public void testVersusJordi_P() {
+        assertEquals("PP0",
+                     -0.94872421,
+                     simple.downgoingRFCoeff.getFreePtoPRefl(flatRP),
+                     0.00001);
+        assertEquals("PS0",
+                     0.411624223,
+                     simple.downgoingRFCoeff.getFreePtoSVRefl(flatRP),
+                     0.00001);
+        assertEquals("SP0",
+                     0.242751762,
+                     simple.downgoingRFCoeff.getFreeSVtoPRefl(flatRP),
+                     0.00001);
+        assertEquals("PS2",
+                     -0.0896627381,
+                     simple.downgoingRFCoeff.getPtoSVRefl(flatRP),
+                     0.00001);
+        assertEquals("PP2",
+                     0.205925092,
+                     simple.downgoingRFCoeff.getPtoPRefl(flatRP),
+                     0.00001);
+        assertEquals("PS2",
+                     -0.0528778173,
+                     simple.downgoingRFCoeff.getSVtoPRefl(flatRP),
+                     0.00001);
+        assertEquals("PP2",
+                     -0.217589691,
+                     simple.downgoingRFCoeff.getSVtoSVRefl(flatRP),
+                     0.00001);
         assertEquals("P", a[0], simple.getAmpP(flatRP), 0.00001);
-    }
-
-    public void testVersusJordi_Ps() {
         assertEquals("Ps", a[1], simple.getAmpPs(flatRP), 0.00001);
-    }
-
-    public void testVersusJordi_PpPs() {
         assertEquals("PpPs", a[2], simple.getAmpPpPs(flatRP), 0.00001);
-    }
-
-    public void testVersusJordi_PsPs() {
         assertEquals("PsPs", a[3], simple.getAmpPsPs(flatRP), 0.00001);
     }
-
-    SimpleSynthReceiverFunction simple;
-
-    double vpCrust = 6.0;
-
-    double vsCrust = 3.5;
-
-    double rhoCrust = 3.0;
-
-    double vpMantle = 7;
-
-    double vsMantle = 4.2;
-
-    double rhoMantle = 4.0;
-
-    float flatRP = 0.03f;
-
-    double[] a = new double[] {0.213547871,  0.0486433432,  0.0977657735, -0.0919868574};
     
-    double rsr0 = 4.734231;
-    double zsz0 = -0.124569595; 
-    double rpz0 = 0.213547871;
+
+
+    public void testJordiB() {
+        double vpCrust = 6.391;
+        double vsCrust = 3.236;
+        double rhoCrust = 2.7;
+        double vpMantle = 8;
+        double vsMantle = 4.5;
+        double rhoMantle = 3.2;
+        float flatRP = 0.0503f;
+        double[] a = new double[] {0.339172453,  0.14355889,  0.168260157, -0.130562127};
+        SimpleSynthReceiverFunction simple = new SimpleSynthReceiverFunction(new StationResult(MockNetworkId.createNetworkID(),
+                                                                                               "test",
+                                                                                               new QuantityImpl(30,
+                                                                                                                UnitImpl.KILOMETER),
+                                                                                               (float)(vpCrust / vsCrust),
+                                                                                               new QuantityImpl(vpCrust,
+                                                                                                                UnitImpl.KILOMETER_PER_SECOND),
+                                                                                               null),
+                                                                             new SamplingImpl(1,
+                                                                                              new TimeInterval(1,
+                                                                                                               UnitImpl.SECOND)),
+                                                                             4096,
+                                                                             rhoCrust,
+                                                                             vpMantle,
+                                                                             vsMantle,
+                                                                             rhoMantle);
+        simple.calculate(flatRP,
+                         ClockUtil.now().getFissuresTime(),
+                         new TimeInterval(0, UnitImpl.SECOND),
+                         MockChannel.createChannel().get_id(),
+                         5);
+        assertEquals("P", a[0], simple.getAmpP(flatRP), 0.00001);
+        assertEquals("Ps", a[1], simple.getAmpPs(flatRP), 0.00001);
+        assertEquals("PpPs", a[2], simple.getAmpPpPs(flatRP), 0.00001);
+        assertEquals("PsPs", a[3], simple.getAmpPsPs(flatRP), 0.00001);
+    }
 }

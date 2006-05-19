dbe91940112f78a53dcdf630d04bb0a9991f2518
crotwell <crotwell@localhost>
2006-05-19 17:15:41 -0400

    [svn r17258] play with topography

--- GMT/WestUS/parseTA_csv.py
+++ GMT/WestUS/parseTA_csv.py
@@ -12,13 +12,35 @@ def mapCleanUp(outFilename):
     except OSError:
 	pass
 
+def etopo(outFilename, proj='M5.5i', region='-126/-114/30/50', shift='', extra='', cptFile='/seis/local/External/GMT/share/cpt/GMT_topo.cpt'):
+    out = ' >> '+outFilename
+    baseGmtArgs = '-J'+proj+' -R'+region+' -P -K '+extra
+    gmt = os.popen('grdimage /data/GMT/Grid/etopo5.grd -T -C'+cptFile+' ' +baseGmtArgs+ out, 'w')
+    gmt.close()
 
-def makeBigMap(dataFile, outFilename, proj='M5.5i', region='-126/-114/30/50', shift='', extra=''):
+def gtopo(outFilename, proj='M5.5i', region='-126/-114/30/50', shift='', extra='', cptFile='/seis/local/External/GMT/share/cpt/GMT_topo.cpt'):
+    out = ' >> '+outFilename
+    baseGmtArgs = '-J'+proj+' -R'+region+' -P -K '+extra
+    gmt = os.popen('grdimage /data/GMT/Grid/gtopo30/w140n40.grd -T -C'+cptFile+' ' +baseGmtArgs+ out, 'w')
+    gmt.close()
+    gmt = os.popen('grdimage /data/GMT/Grid/gtopo30/w140n90.grd -T -C'+cptFile+' ' +baseGmtArgs+ out, 'w')
+    gmt.close()
+
+def crust2(outFilename, proj='M5.5i', region='-126/-114/30/50', shift='', extra='', backgroundGRD='/data/GMT/Grid/Crust2/thickness_180.grd', cptFile='no_green_25_50.cpt'):
+    out = ' >> '+outFilename
+    baseGmtArgs = '-J'+proj+' -R'+region+' -P -K '+extra
+    gmt = os.popen('grdimage '+backgroundGRD+' -T -C'+cptFile+' ' +baseGmtArgs+ out, 'w')
+    gmt.close()
+
+    
+
+def makeBigMap(dataFile, outFilename, proj='M5.5i', region='-126/-114/30/50', shift='', extra='', minEQ=0, backgroundGRD='/data/GMT/Grid/Crust2/thickness_180.grd', cptFile='no_green_25_50.cpt'):
     mapCleanUp(outFilename)
-    makeMap(dataFile, outFilename, proj, region, shift, extra)
+    makeMap(dataFile, outFilename, proj, region, shift, extra, minEQ=minEQ, backgroundGRD=backgroundGRD, cptFile=cptFile)
     ps2pdf(outFilename)
+    print outFilename
 
-def makeMap(dataFile, outFilename, proj='M5.5i', region='-126/-114/30/50', shift='', extra=''):
+def makeMap(dataFile, outFilename, proj='M5.5i', region='-126/-114/30/50', shift='', extra='', vpvs=False, minEQ=0, backgroundGRD='/data/GMT/Grid/Crust2/thickness_180.grd', cptFile='no_green_25_50.cpt'):
     out = ' >> '+outFilename
     gmt = os.popen('gmtset BASEMAP_TYPE=plain')
     gmt.close()
@@ -30,14 +52,13 @@ def makeMap(dataFile, outFilename, proj='M5.5i', region='-126/-114/30/50', shift
 	print 'adding -O'
 	baseGmtArgs = baseGmtArgs + ' -O '
 
-    # makecpt -I -T25/50/1 -Cno_green > no_green_25_50
-    gmt = os.popen('grdimage ../crust2_180.grd -T -Cno_green_25_50.cpt ' +baseGmtArgs+ out, 'w')
-    gmt.close()
+    #crust2(outFilename, proj, region, shift, extra)
+    etopo(outFilename, proj, region, shift, extra+' -O ')
 
     gmt= os.popen('pscoast ' +baseGmtArgs+' -Dh -Na -W '+out, 'w')
     gmt.close()
 
-    gmt= os.popen('psxy '+baseGmtArgs+' -St.2i -Cno_green_25_50.cpt -W'+out, 'w')
+    gmt= os.popen('psxy '+baseGmtArgs+' -St.2i -C'+cptFile+' -W'+out, 'w')
     results = csv.reader(open(dataFile, 'r'))
     mean = 0.0
     numMean = 0
@@ -48,7 +69,7 @@ def makeMap(dataFile, outFilename, proj='M5.5i', region='-126/-114/30/50', shift
 	if ( float(thick) > 50):
 	    #print row[0]+' '+ row[1]+" "+ row[3]+"  "+ row[2]+"  "+row[5]
 	    pass
-	if int(row[12]) >= 5 and thick != '...':
+	if int(row[12]) >= minEQ and thick != '...':
 	    mean = mean +float(thick)
 	    numMean = numMean+1
 	    gmt.write(row[3]+"  "+ row[2]+"  "+thick+"\n")
@@ -57,7 +78,7 @@ def makeMap(dataFile, outFilename, proj='M5.5i', region='-126/-114/30/50', shift
     gmt.close()
     mean = mean / numMean
     print 'mean='+str(mean)+' numMean='+str(numMean)
-    gmt = os.popen('psscale -D6i/7i/5i/.5i -Cno_green_25_50.cpt -B5 -P -O -K '+out, 'w')
+    gmt = os.popen('psscale -D6i/7i/5i/.5i -C'+cptFile+' -B5 -P -O -K '+out, 'w')
     gmt.close()
 
     baseGmtArgs = baseGmtArgs.replace('-K', '')
@@ -66,25 +87,26 @@ def makeMap(dataFile, outFilename, proj='M5.5i', region='-126/-114/30/50', shift
     gmt.close()
 
 
-def makeLonVsThickBoxes(dataFile, outfilePrefix, minLat, maxLat, incLat):
-    xyregion='-126/-114/20/60'
+def makeLonVsThickBoxes(dataFile, outfilePrefix, minLat, maxLat, incLat, minEQ=0, backgroundGRD='/data/GMT/Grid/Crust2/thickness_180.grd', cptFile='no_green_25_50.cpt'):
+    xyregion='-124/-114/20/60'
     for lat in range(minLat, maxLat, incLat):
 	outFilename = outfilePrefix+str(lat)+'.ps'
 	mapCleanUp(outFilename)
 	out = ' >> '+outFilename
-	gmt= os.popen('psxy -B2WESn -P -JX6i/-6i -R'+xyregion+' -St.2i -G0 -K '+out, 'w')
+	gmt= os.popen('psxy -B2WESn -P -Ey -JX6i/-6i -R'+xyregion+' -Cno_green_25_50.cpt -St.2i -G0 -K '+out, 'w')
 	results = csv.reader(open(dataFile, 'r'))
 	mean = 0.0
 	numMean = 0
 	for row in results:
 	    if row[0].startswith('#') or  row[5] == '' or  row[5] == '...':
 		continue
-	    thick = row[5].replace(' km','') 
+	    thick = float(row[5].replace(' km','')) 
+	    thickErr = float(row[6].replace(' km','')) 
 	    stalat = float(row[2])
-	    if  int(row[12]) >= 5 and stalat > lat and stalat < (lat+incLat):
+	    if  int(row[12]) >= minEQ and stalat >= lat and stalat < (lat+incLat):
 		mean = mean +float(thick)
 		numMean = numMean+1
-		gmt.write(row[3]+"  "+thick+" "+row[7]+"\n") 
+		gmt.write( "%s %f %f %f %f\n" % (row[3], thick, thick, thickErr, thickErr)) 
 	gmt.close()
 	if numMean > 0 :
 	    mean = mean / numMean
@@ -100,12 +122,12 @@ def makeLonVsThickBoxes(dataFile, outfilePrefix, minLat, maxLat, incLat):
 	    thick = row[5].replace(' km','') 
 	    stalat = float(row[2])
 	    #print "%s.%s %s" % (row[0],row[1],row[12])
-	    if int(row[12]) >= 5 and stalat > lat and stalat < (lat+incLat):
+	    if int(row[12]) >= minEQ and stalat >= lat and stalat < (lat+incLat):
 		gmt.write(row[3]+" "+thick+" 8 0 4 BL ."+row[0]+'.'+row[1]+"\n")
 	gmt.close()	
 
-	mapRegion='-126/-114/'+str(lat)+'/'+str(lat+incLat)
-	makeMap(dataFile, outFilename, 'M6i', mapRegion,'-Y6.5i','-O')
+	mapRegion='-124/-114/'+str(lat)+'/'+str(lat+incLat)
+	makeMap(dataFile, outFilename, 'M6i', mapRegion,'-Y6.5i','-O', minEQ=minEQ)
 	ps2pdf(outFilename)
 	    
 def ps2pdf(outFilename):
@@ -123,9 +145,18 @@ gmtset.close()
 outFilename = 'mapTA.ps'
 #makeMap('TA.result', outFilename)
 
-makeBigMap('allWestCoast.csv', 'mapAll.ps', 'M5.5i','-126/-114/36/38' )
+#makeBigMap('allWestCoast.csv', 'mapAll.ps', 'M5.5i','-126/-114/32/49' )
 #ps2pdf('mapAll.ps')
 
-makeLonVsThickBoxes('allWestCoast.csv', 'lonBox_', 32, 42, 2)
+#makeLonVsThickBoxes('allWestCoast.csv', 'lonBox_', 32, 49, 1, minEQ=5)
 
-makeLonVsThickBoxes('TAresult.csv', 'TAlonBox_', 32, 42, 2)
+# grd2cpt /data/GMT/Grid/gtopo30/w140n40.grd -Crelief -Z > topo.cpt
+    # makecpt -I -T25/50/1 -Cno_green > no_green_25_50
+
+
+makeBigMap('allWestCoast.csv', 'topoAll.ps', 'M5.5i','-126/-114/32/49', minEQ=5)
+
+#makeLonVsThickBoxes('TAresult.csv', 'TAlonBox_', 32, 49, 2, minEQ=5)
+
+
+#makeLonVsThickBoxes('allWestCoast.csv', 'topolonBox_', 32, 49, 1, minEQ=5, backgroundGRD='/data/GMT/Grid/gtopo30', cptFile='topo.cpt')

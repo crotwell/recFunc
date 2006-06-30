8a0a0e77a35a40abdb29384b0699a2586d9bac83
crotwell <crotwell@localhost>
2006-06-30 16:51:02 -0400

    [svn r17699] changes for poster?

--- GMT/WestUS/getData.sh
+++ GMT/WestUS/getData.sh
@@ -1,4 +1,4 @@
 
 curl  -o allWestCoast.csv.new 'http://www.seis.sc.edu/ears/stationLatLonBox.txt?minLat=30&maxLat=50&minLon=-126&maxLon=-114'
 
-curl -o TAresults.csv.new 'http://www.seis.sc.edu/ears_dev/stationList.txt?netdbid=62&gaussian=2.5'
+curl -o TAresult.csv.new 'http://www.seis.sc.edu/ears_dev/stationList.txt?netdbid=62&gaussian=2.5'
--- GMT/WestUS/parseTA_csv.py
+++ GMT/WestUS/parseTA_csv.py
@@ -40,7 +40,7 @@ def makeBigMap(dataFile, outFilename, proj='M5.5i', region='-126/-114/30/50', sh
     ps2pdf(outFilename)
     print outFilename
 
-def makeMap(dataFile, outFilename, proj='M5.5i', region='-126/-114/30/50', shift='', extra='', vpvs=False, minEQ=0, backgroundGRD='/data/GMT/Grid/Crust2/thickness_180.grd', cptFile='no_green_25_50.cpt'):
+def makeMap(dataFile, outFilename, proj='M5.5i', region='-126/-114/30/50', shift='', extra='', vpvs=False, minEQ=0, backgroundGRD='/data/GMT/Grid/Crust2/thickness_180.grd', cptFile='no_green_25_50.cpt', onlyOneNet='', labelSta=False):
     out = ' >> '+outFilename
     gmt = os.popen('gmtset BASEMAP_TYPE=plain')
     gmt.close()
@@ -52,8 +52,8 @@ def makeMap(dataFile, outFilename, proj='M5.5i', region='-126/-114/30/50', shift
 	print 'adding -O'
 	baseGmtArgs = baseGmtArgs + ' -O '
 
-    #crust2(outFilename, proj, region, shift, extra)
-    etopo(outFilename, proj, region, shift, extra+' -O ')
+    #crust2(outFilename, proj, region, shift, extra+' -O ')
+    #etopo(outFilename, proj, region, shift, extra+' -O ')
 
     gmt= os.popen('pscoast ' +baseGmtArgs+' -Dh -Na -W '+out, 'w')
     gmt.close()
@@ -63,23 +63,37 @@ def makeMap(dataFile, outFilename, proj='M5.5i', region='-126/-114/30/50', shift
     mean = 0.0
     numMean = 0
     for row in results: 
-	if row[0].startswith('#') or  row[5] == '' or  row[5] == '...':
+	if row[0].startswith('#'):
+	    continue
+	if onlyOneNet != '' and onlyOneNet != row[0]:
 	    continue
 	thick = row[5].replace(' km','')
-	if ( float(thick) > 50):
+	if (thick != '' and  thick != '...' and float(thick) > 50):
 	    #print row[0]+' '+ row[1]+" "+ row[3]+"  "+ row[2]+"  "+row[5]
 	    pass
-	if int(row[12]) >= minEQ and thick != '...':
+	if thick != '' and  thick != '...' and int(row[12]) >= minEQ:
 	    mean = mean +float(thick)
 	    numMean = numMean+1
 	    gmt.write(row[3]+"  "+ row[2]+"  "+thick+"\n")
-	else:
-	    gmt.write(row[3]+"  "+ row[2]+"  NaN\n") 
+	elif onlyOneNet != '':
+	    gmt.write(row[3]+"  "+ row[2]+"  NaN\n")
     gmt.close()
     mean = mean / numMean
     print 'mean='+str(mean)+' numMean='+str(numMean)
     gmt = os.popen('psscale -D6i/7i/5i/.5i -C'+cptFile+' -B5 -P -O -K '+out, 'w')
     gmt.close()
+#label
+    if labelSta:
+       gmt= os.popen('pstext '+baseGmtArgs+'  -W'+out, 'w')
+       results = csv.reader(open(dataFile, 'r'))
+       for row in results: 
+           if row[0].startswith('#'):
+               continue
+           if onlyOneNet != '' and onlyOneNet != row[0]:
+               continue
+           thick = row[5].replace(' km','')
+           gmt.write(row[3]+"  "+ row[2]+" 8 0 4 BL ."+row[0]+'.'+row[1]+"\n") 
+       gmt.close()
 
     baseGmtArgs = baseGmtArgs.replace('-K', '')
     gmt= os.popen('psxy '+baseGmtArgs+' -St.003i -G255 '+out, 'w')
@@ -87,7 +101,7 @@ def makeMap(dataFile, outFilename, proj='M5.5i', region='-126/-114/30/50', shift
     gmt.close()
 
 
-def makeLonVsThickBoxes(dataFile, outfilePrefix, minLat, maxLat, incLat, minEQ=0, backgroundGRD='/data/GMT/Grid/Crust2/thickness_180.grd', cptFile='no_green_25_50.cpt'):
+def makeLonVsThickBoxes(dataFile, outfilePrefix, minLat, maxLat, incLat, minEQ=0, backgroundGRD='/data/GMT/Grid/Crust2/thickness_180.grd', cptFile='no_green_25_50.cpt', mapLabelSta=False, onlyOneNet=''):
     xyregion='-124/-114/20/60'
     for lat in range(minLat, maxLat, incLat):
 	outFilename = outfilePrefix+str(lat)+'.ps'
@@ -98,15 +112,22 @@ def makeLonVsThickBoxes(dataFile, outfilePrefix, minLat, maxLat, incLat, minEQ=0
 	mean = 0.0
 	numMean = 0
 	for row in results:
-	    if row[0].startswith('#') or  row[5] == '' or  row[5] == '...':
+	    if row[0].startswith('#'):
 		continue
+	    if onlyOneNet != '' and onlyOneNet != row[0]:
+		continue
+	    if row[5] == '' or  row[5] == '...':
+		row[5] = '59'
+		row[6] = '0'
+		row[12] = -1
 	    thick = float(row[5].replace(' km','')) 
 	    thickErr = float(row[6].replace(' km','')) 
 	    stalat = float(row[2])
-	    if  int(row[12]) >= minEQ and stalat >= lat and stalat < (lat+incLat):
-		mean = mean +float(thick)
-		numMean = numMean+1
-		gmt.write( "%s %f %f %f %f\n" % (row[3], thick, thick, thickErr, thickErr)) 
+	    if stalat >= lat and stalat < (lat+incLat):
+		if int(row[12]) >= minEQ:
+		    mean = mean +float(thick)
+		    numMean = numMean+1
+		    gmt.write( "%s %f %f %f %f\n" % (row[3], thick, thick, thickErr, thickErr)) 
 	gmt.close()
 	if numMean > 0 :
 	    mean = mean / numMean
@@ -117,24 +138,30 @@ def makeLonVsThickBoxes(dataFile, outfilePrefix, minLat, maxLat, incLat, minEQ=0
 	mean = 0.0
 	numMean = 0
 	for row in results:
-	    if row[0].startswith('#') or  row[5] == '' or  row[5] == '...':
+	    if row[0].startswith('#'):
+		continue
+	    if onlyOneNet != '' and onlyOneNet != row[0]:
 		continue
 	    thick = row[5].replace(' km','') 
+	    if row[5] == '' or  row[5] == '...':
+		row[5] = 59
+		row[12] = '-1'
 	    stalat = float(row[2])
 	    #print "%s.%s %s" % (row[0],row[1],row[12])
-	    if int(row[12]) >= minEQ and stalat >= lat and stalat < (lat+incLat):
-		gmt.write(row[3]+" "+thick+" 8 0 4 BL ."+row[0]+'.'+row[1]+"\n")
+	    if stalat >= lat and stalat < (lat+incLat):
+		if int(row[12]) >= minEQ:
+		    gmt.write(row[3]+" "+thick+" 8 0 4 BL ."+row[0]+'.'+row[1]+"\n")
 	gmt.close()	
 
 	mapRegion='-124/-114/'+str(lat)+'/'+str(lat+incLat)
-	makeMap(dataFile, outFilename, 'M6i', mapRegion,'-Y6.5i','-O', minEQ=minEQ)
+	makeMap(dataFile, outFilename, 'M6i', mapRegion,'-Y6.5i','-O', minEQ=minEQ, labelSta=mapLabelSta, onlyOneNet=onlyOneNet)
 	ps2pdf(outFilename)
 	    
 def ps2pdf(outFilename):
     ps2pdf = os.popen('pstopdf '+outFilename)
     ps2pdf.close()
     try:
-	os.remove(outFilename)
+	#os.remove(outFilename)
 	pass
     except OSError:
 	pass
@@ -143,20 +170,22 @@ gmtset = os.popen('gmtset OUTPUT_DEGREE_FORMAT=-ddd PLOT_DEGREE_FORMAT=-ddd')
 gmtset.close()
 
 outFilename = 'mapTA.ps'
-#makeMap('TA.result', outFilename)
+mapCleanUp(outFilename)
+makeMap('TAresult.csv', outFilename, 'M6i','-124/-114/32/49', onlyOneNet='TA' , minEQ=5)
+ps2pdf( outFilename)
 
-#makeBigMap('allWestCoast.csv', 'mapAll.ps', 'M5.5i','-126/-114/32/49' )
-#ps2pdf('mapAll.ps')
+makeBigMap('allWestCoast.csv', 'mapAll.ps', 'M6i','-124/-114/32/49' )
+ps2pdf('mapAll.ps')
 
 #makeLonVsThickBoxes('allWestCoast.csv', 'lonBox_', 32, 49, 1, minEQ=5)
 
 # grd2cpt /data/GMT/Grid/gtopo30/w140n40.grd -Crelief -Z > topo.cpt
-    # makecpt -I -T25/50/1 -Cno_green > no_green_25_50
+# makecpt -I -T25/50/1 -Cno_green > no_green_25_50
 
 
-makeBigMap('allWestCoast.csv', 'topoAll.ps', 'M5.5i','-126/-114/32/49', minEQ=5)
+#makeBigMap('allWestCoast.csv', 'topoAll.ps', 'M5.5i','-126/-114/32/49', minEQ=5)
 
-#makeLonVsThickBoxes('TAresult.csv', 'TAlonBox_', 32, 49, 2, minEQ=5)
+#makeLonVsThickBoxes('TAresult.csv', 'TAlonBox_', 32, 49, 2, onlyOneNet='TA' , minEQ=5, mapLabelSta=True)
 
 
 #makeLonVsThickBoxes('allWestCoast.csv', 'topolonBox_', 32, 49, 1, minEQ=5, backgroundGRD='/data/GMT/Grid/gtopo30', cptFile='topo.cpt')

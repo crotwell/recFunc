07aedcfb2392caac32ed016cfc814e4f909bbf37
crotwell <crotwell@localhost>
2006-07-07 16:34:29 -0400

    [svn r17798] use name and dns to be like other servers

--- src/edu/sc/seis/receiverFunction/server/RecFuncCacheCheck.java
+++ src/edu/sc/seis/receiverFunction/server/RecFuncCacheCheck.java
@@ -8,8 +8,10 @@ import edu.iris.Fissures.IfNetwork.ChannelId;
 import edu.sc.seis.IfReceiverFunction.IterDeconConfig;
 import edu.sc.seis.IfReceiverFunction.RecFuncCacheHelper;
 import edu.sc.seis.IfReceiverFunction.RecFuncCacheOperations;
+import edu.sc.seis.fissuresUtil.display.configuration.DOMHelper;
 import edu.sc.seis.fissuresUtil.namingService.FissuresNamingService;
 import edu.sc.seis.receiverFunction.RecFuncProcessor;
+import edu.sc.seis.rev.RevUtil;
 import edu.sc.seis.sod.ChannelGroup;
 import edu.sc.seis.sod.CommonAccess;
 import edu.sc.seis.sod.ConfigurationException;
@@ -28,16 +30,8 @@ public class RecFuncCacheCheck implements EventVectorSubsetter {
 
     public RecFuncCacheCheck(Element config) throws ConfigurationException {
         deconConfig = RecFuncProcessor.parseIterDeconConfig(config);
-        String dns = "edu/sc/seis";
-        String serverName = "Ears";
-        Element dnsElement = SodUtil.getElement(config, "serverDNS");
-        if (dnsElement != null) {
-             dns = SodUtil.getNestedText(dnsElement);
-        }
-        Element serverElement = SodUtil.getElement(config, "serverName");
-        if (serverElement != null) {
-             serverName = SodUtil.getNestedText(serverElement);
-        }
+        String dns = DOMHelper.extractText(config, "dns", "edu/sc/seis");
+        String serverName = DOMHelper.extractText(config, "name", "Ears");
         try {
             FissuresNamingService fisName = CommonAccess.getCommonAccess().getFissuresNamingService();
             cache = new NSRecFuncCache(dns, serverName, fisName);

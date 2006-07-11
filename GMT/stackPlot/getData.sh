4639550e403451c7f1c044e494f78c1fb9d48fe6
crotwell <crotwell@localhost>
2006-07-11 16:19:37 -0400

    [svn r17833] add residual

--- GMT/stackPlot/getData.sh
+++ GMT/stackPlot/getData.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 curl -o data.xyz 'http://www.seis.sc.edu/ears/sumHKStackAsXYZ.txt?netcode=TA&stacode=M06C&minPercentMatch=80&smallestH=25.0&gaussian=2.5'
+
+curl -o synth.xyz 'http://www.seis.sc.edu/ears_dev/synthHKAsXYZ.txt?netcode=TA&stacode=M06C&minPercentMatch=80&smallestH=25.0&gaussian=2.5'
+
+curl -o residual.xyz 'http://www.seis.sc.edu/ears_dev/complexResidualAsXYZ.txt?netcode=TA&stacode=M06C&minPercentMatch=80&smallestH=25.0&gaussian=2.5'
--- GMT/stackPlot/makePlot.sh
+++ GMT/stackPlot/makePlot.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-OUT=image.ps
+TYPELIST="data synth residual"
 
-xyz2grd data.xyz -Gdata.grd -I.0025/.25 -H -R1.6/2.1/25/69.75
+for type in $TYPELIST ; do
+    OUT=${type}.ps
+    xyz2grd ${type}.xyz -G${type}.grd -I.0025/.25 -H -R1.6/2.1/25/69.75
 
-# if you change data....
-# to get min val run
-# grdinfo data.grd | grep z_max 
-# and put 0/max value into grd2cpt -L option
+    # if you change ${type}....
+    # to get min val run
+    # grdinfo ${type}.grd | grep z_max 
+    # and put 0/max value into grd2cpt -L option
 
-rm -f grdcolor.cpt
-grd2cpt data.grd -Cwysiwyg -L0/0.0266749 > grdcolor.cpt
+    rm -f grdcolor.cpt
+    grd2cpt ${type}.grd -Cwysiwyg -L0/0.0266749 > grdcolor.cpt
 
-grdimage data.grd -P -JX3i/-3i -B.1/5 -R1.6/2.1/25/70 -Cgrdcolor.cpt > ${OUT}
+   grdimage ${type}.grd -P -JX3i/-3i -B.1/5 -R1.6/2.1/25/70 -Cgrdcolor.cpt > ${OUT}
+    ps2pdf ${OUT}
 
-ps2pdf ${OUT}
+
+done

40ebd752e0b3831733e91e97d6a5d87fb16acfbe
groves <groves@localhost>
2006-11-15 14:48:18 -0500

    [svn r18490] just create a VestingNetworkDC directly

--- src/edu/sc/seis/receiverFunction/server/NetworkDBPopulate.java
+++ src/edu/sc/seis/receiverFunction/server/NetworkDBPopulate.java
@@ -8,36 +8,32 @@ import edu.iris.Fissures.IfNetwork.NetworkAccess;
 import edu.iris.Fissures.IfNetwork.NetworkDCOperations;
 import edu.iris.Fissures.IfNetwork.NetworkNotFound;
 import edu.iris.Fissures.model.AllVTFactory;
-import edu.sc.seis.fissuresUtil.cache.BulletproofVestFactory;
+import edu.sc.seis.fissuresUtil.cache.VestingNetworkDC;
 import edu.sc.seis.fissuresUtil.database.ConnMgr;
 import edu.sc.seis.fissuresUtil.database.network.JDBCNetwork;
 import edu.sc.seis.fissuresUtil.namingService.FissuresNamingService;
-import edu.sc.seis.fissuresUtil.simple.Initializer;
 
 /**
  * @author crotwell Created on Oct 5, 2004
  */
 public class NetworkDBPopulate {
 
-    public static void main(String[] args) throws NetworkNotFound, SQLException, IOException {
-
+    public static void main(String[] args) throws NetworkNotFound,
+            SQLException, IOException {
         Properties props = StackSummary.loadProps(args);
-        org.omg.CORBA_2_3.ORB orb =
-            (org.omg.CORBA_2_3.ORB)ORB.init(args, props);
-        
+        org.omg.CORBA_2_3.ORB orb = (org.omg.CORBA_2_3.ORB)ORB.init(args, props);
         // register valuetype factories
         edu.iris.Fissures.model.AllVTFactory vt = new AllVTFactory();
         vt.register(orb);
         ConnMgr.setDB(ConnMgr.POSTGRES);
         FissuresNamingService fisName = new FissuresNamingService(orb);
         fisName.setNameServiceCorbaLoc(props.getProperty(FissuresNamingService.CORBALOC_PROP));
-        NetworkDCOperations netDC = BulletproofVestFactory.vestNetworkDC("edu/iris/dmc",
-                                                                         "IRIS_NetworkDC",
-                                                                         fisName);
+        NetworkDCOperations netDC = new VestingNetworkDC("edu/iris/dmc",
+                                                         "IRIS_NetworkDC",
+                                                         fisName);
         NetworkAccess[] nets = netDC.a_finder().retrieve_by_code(args[0]);
-        
         JDBCNetwork jdbcNet = new JDBCNetwork();
         int dbid = jdbcNet.put(nets[0].get_attributes());
-        System.out.println("Done "+args[0]+" "+dbid);
+        System.out.println("Done " + args[0] + " " + dbid);
     }
 }

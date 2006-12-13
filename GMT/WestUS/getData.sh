d0ba325fe1d2cc42921342ec0a6f4e5914533f88
crotwell <crotwell@localhost>
2006-12-13 10:31:29 -0500

    [svn r18612] rename files after retrieved

--- GMT/WestUS/getData.sh
+++ GMT/WestUS/getData.sh
@@ -1,4 +1,9 @@
 
-curl  -o allWestCoast.csv.new 'http://www.seis.sc.edu/ears/stationLatLonBox.txt?minLat=30&maxLat=50&minLon=-126&maxLon=-114'
+curl -m 900  -o allWestCoast.csv.new 'http://www.seis.sc.edu/ears/stationLatLonBox.txt?minLat=30&maxLat=50&minLon=-126&maxLon=-114'
+rm  allWestCoast.csv
+mv  allWestCoast.csv.new  allWestCoast.csv
 
-curl -o TAresult.csv.new 'http://www.seis.sc.edu/ears_dev/stationList.txt?netdbid=62&gaussian=2.5'
+curl -m 900 -o TAresult.csv.new 'http://www.seis.sc.edu/ears_dev/stationList.txt?netdbid=62&gaussian=2.5'
+
+rm TAresult.csv
+mv TAresult.csv.new TAresult.csv

d200529da0dd22d6f6a7e211d85f4513cdd704dc
crotwell <crotwell@localhost>
2007-01-25 13:48:59 -0500

    [svn r18761] clean up plotting

--- GMT/Moma/plot.py
+++ GMT/Moma/plot.py
@@ -3,16 +3,17 @@
 import csv, os
 
 def ps2pdf(outFilename):
-    ps2pdf = os.popen('pstopdf '+outFilename)
-    ps2pdf.close()
+    os.popen('eps2eps '+outFilename+' '+outFilename.replace('.ps','.eps')).close()
+    os.popen('epstopdf '+outFilename.replace('.ps','.eps')).close()
     try:
-	os.remove(outFilename)
+#	os.remove(outFilename)
 	pass
     except OSError:
 	pass
 
 def plotGaussH(outFilename, gauss, color='0/0/0'):
     out = ' >> '+outFilename
+    labelShift='1.25i'
     dialect = csv.get_dialect('excel')
     results = csv.reader(open('moma'+gauss+'.csv', 'r'), dialect=dialect)
     row = results.next()
@@ -22,38 +23,38 @@ def plotGaussH(outFilename, gauss, color='0/0/0'):
 	rows.append(row)
 #    gmt= os.popen('psxy -P -JX -R -G'+color+' -O -K '+out, 'w')
 #    for row in rows:
-#	gmt.write("%s %s\n" % (row[4], row[5]))
+#	gmt.write("%s %s\n" % (row[4], row[6].replace(' km','')))
 #    gmt.close()
     gmt= os.popen('psxy -P -JX -R -Sc.08i -G'+color+' -O -K '+out, 'w')
     for row in rows:
-	gmt.write("%s %s\n" % (row[4], row[5]))
+        print "row %s %s\n" % (row[4], row[6].replace(' km',''))
+	gmt.write("%s %s\n" % (row[4], row[6].replace(' km','')))
     gmt.close()
     gmt= os.popen('psxy -P -JX -R -St.1i -G255/0/0 -O -K '+out, 'w')
     for row in rows:
-	gmt.write("%s %s\n" % (row[4], row[14]))
+	gmt.write("%s %s\n" % (row[4], row[15].replace(' km','')))
     gmt.close()
-    gmt= os.popen('pstext -P -JX -R -O -K -W'+out, 'w')
+    gmt= os.popen('pstext -Y'+labelShift+' -P -JX -R -O -K -W'+out, 'w')
     for row in rows:
 	thick = row[5].replace(' km','')
-	thick = '25'
-	print(row[4]+"  "+ thick+" 12 90 4 ML "+row[0]+'.'+row[1])
+	thick = '30'
 	gmt.write(row[4]+"  "+ thick+" 12 90 4 ML "+row[0]+'.'+row[1]+"\n")
     gmt.close()
+    os.popen('pstext -Y-'+labelShift+' -P -JX -R -O -K -W'+out, 'w').close()
     # plot stations without a moma result in array
     # also do pstext from array to get all stations
     results = csv.reader(open('moma'+gauss+'_nocomp.csv', 'r'), dialect=dialect)
-    row = results.next()
-    row = results.next()
     rows = []
     for row in results:
 	rows.append(row)
-    gmt= os.popen('pstext -P -JX -R -O -K -W'+out, 'w')
+    gmt= os.popen('pstext -Y'+labelShift+' -P -JX -R -O -K -W'+out, 'w')
     for row in rows:
 	if (row[1] != 'MO18'):
 	    thick = row[5].replace(' km','')
-	    thick = '25'
+	    thick = '30'
 	    gmt.write(row[3]+"  "+ thick+" 12 90 4 ML "+row[0]+'.'+row[1]+"\n")
     gmt.close()
+    os.popen('pstext -Y-'+labelShift+' -P -JX -R -O -K -W'+out, 'w').close()
     gmt= os.popen('psxy -P -JX -R -Sc.08i -G'+color+' -O -K '+out, 'w')
     for row in rows:
 	if row[5] != '':
@@ -71,7 +72,7 @@ def legend(outFilename, offset, symbol, color, text):
 
 def makeHVsLon(outFilename):
     out = ' >> '+outFilename
-    gmt= os.popen('psxy -B2WESn -P -JX6i/-8i -R-94/-70/20/51 -K '+out, 'w')
+    gmt= os.popen('psxy -B2WESn -P -JX6i/-6i -R-94/-70/25/51 -K '+out, 'w')
     gmt.close()
     print 'aaaaaa'
 
@@ -79,13 +80,13 @@ def makeHVsLon(outFilename):
     plotGaussH(outFilename, '1.0', '0/0/255')
 #    plotGaussH(outFilename, '0.7', '0/255/0')
 #    plotGaussH(outFilename, '0.4', '0/255/255')
-    gmt= os.popen('psxy -P -JX -R -O -K -Y2 '+out, 'w')
-    gmt.close()
-    legend(outFilename, '21', 't.1i', '255/0/0', 'Moma')
-    legend(outFilename, '21.75', 'c.08i', '0/0/0', '2.5')
-    legend(outFilename, '22.5', 'c.08i', '0/0/255', '1.0')
-#    legend(outFilename, '23.25', 'c.08i', '0/255/0', '0.7')
-#    legend(outFilename, '24', 'c.08i', '0/255/255', '0.4')
+#    gmt= os.popen('psxy -P -JX -R -O -K -Y2 '+out, 'w')
+#    gmt.close()
+    legend(outFilename, '26', 't.1i', '255/0/0', 'MOMA')
+    legend(outFilename, '26.75', 'c.08i', '0/0/0', 'Gauss 2.5')
+    legend(outFilename, '27.5', 'c.08i', '0/0/255', 'Gauss 1.0')
+#    legend(outFilename, '28.25', 'c.08i', '0/255/0', 'Gauss 0.7')
+#    legend(outFilename, '29', 'c.08i', '0/255/255', 'Gauss 0.4')
 
     gmt= os.popen('psxy -P -JX -R -O '+out, 'w')
     gmt.close()

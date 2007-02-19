7cd246b9e154184fd208cf9ce903140c3df059d2
crotwell <crotwell@localhost>
2007-02-19 14:30:08 -0500

    [svn r18852] use global macro ffor station listing

--- src/edu/sc/seis/receiverFunction/web/Overview.java
+++ src/edu/sc/seis/receiverFunction/web/Overview.java
@@ -7,6 +7,9 @@ import java.util.HashMap;
 import java.util.Iterator;
 import javax.servlet.http.HttpServletRequest;
 import edu.iris.Fissures.model.MicroSecondDate;
+import edu.iris.Fissures.model.TimeInterval;
+import edu.iris.Fissures.model.UnitImpl;
+import edu.sc.seis.fissuresUtil.chooser.ClockUtil;
 import edu.sc.seis.fissuresUtil.database.NotFound;
 import edu.sc.seis.receiverFunction.SumHKStack;
 import edu.sc.seis.rev.RevUtil;
@@ -16,70 +19,72 @@ import edu.sc.seis.sod.velocity.network.VelocityStation;
 
 public class Overview extends StationList {
 
-    public Overview() throws SQLException, ConfigurationException, Exception {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	public Overview() throws SQLException, ConfigurationException, Exception {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
-    public ArrayList getStations(HttpServletRequest req, RevletContext context)
-            throws SQLException, NotFound {
-        checkDataLoaded(req);
-        int minEQ = RevUtil.getInt("minEQ", req, 2);
-        HashMap out = new HashMap();
-        for(Iterator iter = data.keySet().iterator(); iter.hasNext();) {
-            VelocityStation key = (VelocityStation)iter.next();
-            SumHKStack stack = (SumHKStack)data.get(key);
-            if(stack.getNumEQ() >= minEQ) {
-                out.put(key, stack);
-            }
-        }
-        return new ArrayList(out.keySet());
-    }
+	public ArrayList getStations(HttpServletRequest req, RevletContext context)
+			throws SQLException, NotFound {
+		checkDataLoaded(req);
+		int minEQ = RevUtil.getInt("minEQ", req, 2);
+		HashMap out = new HashMap();
+		for (Iterator iter = data.keySet().iterator(); iter.hasNext();) {
+			VelocityStation key = (VelocityStation) iter.next();
+			SumHKStack stack = (SumHKStack) data.get(key);
+			if (stack.getNumEQ() >= minEQ) {
+				out.put(key, stack);
+			}
+		}
+		return new ArrayList(out.keySet());
+	}
 
-    public HashMap getSummaries(HttpServletRequest req,
-                                ArrayList stationList,
-                                RevletContext context) throws SQLException,
-            IOException {
-        checkDataLoaded(req);
-        HashMap out = new HashMap();
-        for(Iterator iter = stationList.iterator(); iter.hasNext();) {
-            Object key = iter.next();
-            out.put(key, data.get(key));
-        }
-        return out;
-    }
+	public HashMap getSummaries(HttpServletRequest req, ArrayList stationList,
+			RevletContext context) throws SQLException, IOException {
+		checkDataLoaded(req);
+		HashMap out = new HashMap();
+		for (Iterator iter = stationList.iterator(); iter.hasNext();) {
+			Object key = iter.next();
+			out.put(key, data.get(key));
+		}
+		return out;
+	}
 
-    public String getVelocityTemplate(HttpServletRequest req) {
-        if(req.getServletPath().endsWith(".txt")) {
-            return "overview_txt.vm";
-        } else if(req.getServletPath().endsWith(".html")) {
-            return "overview_html.vm";
-        } else {
-            throw new RuntimeException("Unknown URL: " + req.getRequestURI());
-        }
-    }
+	public String getVelocityTemplate(HttpServletRequest req) {
+		if (req.getServletPath().endsWith(".txt")) {
+			return "overview_txt.vm";
+		} else if (req.getServletPath().endsWith(".html")) {
+			return "overview_html.vm";
+		} else {
+			throw new RuntimeException("Unknown URL: " + req.getRequestURI());
+		}
+	}
 
-    void checkDataLoaded(HttpServletRequest req) throws SQLException {
-        if(data == null) {
-            data = new HashMap();
-            try {
-                ArrayList summaryList = jdbcSumHKStack.getAllWithoutData();
-                for(Iterator iter = summaryList.iterator(); iter.hasNext();) {
-                    SumHKStack stack = (SumHKStack)iter.next();
-                    data.put(new VelocityStation(stack.getChannel().my_site.my_station),
-                             stack);
-                }
-            } catch(NotFound e) {
-                // I don't think this should ever happen
-                throw new RuntimeException(e);
-            } catch(IOException e) {
-                // bad database problem if this happens
-                throw new RuntimeException(e);
-            }
-        }
-    }
+	synchronized void checkDataLoaded(HttpServletRequest req)
+			throws SQLException {
+		if (data == null
+				|| loadtime.subtract(ClockUtil.now()).greaterThan(CACHE_TIME)) {
+			data = new HashMap();
+			try {
+				ArrayList summaryList = jdbcSumHKStack.getAllWithoutData();
+				for (Iterator iter = summaryList.iterator(); iter.hasNext();) {
+					SumHKStack stack = (SumHKStack) iter.next();
+					data.put(new VelocityStation(
+							stack.getChannel().my_site.my_station), stack);
+				}
+			} catch (NotFound e) {
+				// I don't think this should ever happen
+				throw new RuntimeException(e);
+			} catch (IOException e) {
+				// bad database problem if this happens
+				throw new RuntimeException(e);
+			}
+		}
+	}
 
-    HashMap data = null;
-    
-    MicroSecondDate loadtime;
+	HashMap data = null;
+
+	MicroSecondDate loadtime;
+
+	TimeInterval CACHE_TIME = new TimeInterval(24, UnitImpl.HOUR);
 }

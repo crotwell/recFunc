713b1533e4078a7003921fac0982fb9d92d48fd1
crotwell <crotwell@localhost>
2008-09-04 09:13:24 -0400

    [svn r19884] clean up

--- src/edu/sc/seis/receiverFunction/server/RecFuncCacheController.java
+++ src/edu/sc/seis/receiverFunction/server/RecFuncCacheController.java
@@ -7,28 +7,28 @@ import edu.sc.seis.cormorant.AbstractController;
 import edu.sc.seis.fissuresUtil.hibernate.AbstractHibernateDB;
 
 /**
- * @author crotwell
- * Created on Jan 18, 2005
+ * @author crotwell Created on Jan 18, 2005
  */
 public class RecFuncCacheController extends AbstractController {
 
     /**
-     *
+     * 
      */
-    public RecFuncCacheController(Properties confProps, String serverPropName,
-            ORB orb) throws Exception {
+    public RecFuncCacheController(Properties confProps,
+                                  String serverPropName,
+                                  ORB orb) throws Exception {
         super(confProps, serverPropName, orb);
-        impl = new RecFuncCacheImpl(confProps.getProperty(getPropertryPrefix()+"databaseURL"), confProps.getProperty(getPropertryPrefix()+"dataloc"));
+        impl = new RecFuncCacheImpl(confProps.getProperty(getPropertryPrefix()
+                + "databaseURL"), confProps.getProperty(getPropertryPrefix()
+                + "dataloc"), confProps);
     }
 
-    
-    
     public void destroy() throws Exception {
         super.destroy();
-        // maybe should have something like? 
-        //AbstractHibernateDB.close();
+        // maybe should have something like?
+        // AbstractHibernateDB.close();
     }
-    
+
     public Servant getServant() {
         return impl;
     }
@@ -36,6 +36,6 @@ public class RecFuncCacheController extends AbstractController {
     public String getInterfaceName() {
         return NSRecFuncCache.interfaceName;
     }
-    
+
     private RecFuncCacheImpl impl;
 }

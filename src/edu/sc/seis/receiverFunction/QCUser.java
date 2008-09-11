36595461c3be0522c66262dfcf6e52493ad0759f
crotwell <crotwell@localhost>
2008-09-11 13:11:49 -0400

    [svn r19909] work on user qc

--- src/edu/sc/seis/receiverFunction/QCUser.java
+++ src/edu/sc/seis/receiverFunction/QCUser.java
@@ -1,11 +1,34 @@
 package edu.sc.seis.receiverFunction;
 
-
 public class QCUser {
+
+    protected long dbid;
     
     protected String username;
-    
+
     protected String passwordHash;
-    
-    
+
+    protected void setUsername(String username) {
+        this.username = username;
+    }
+
+    protected void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public long getDbid() {
+        return dbid;
+    }
+
+    protected void setDbid(long dbid) {
+        this.dbid = dbid;
+    }
 }
--- src/edu/sc/seis/receiverFunction/UserReceiverFunctionQC.java
+++ src/edu/sc/seis/receiverFunction/UserReceiverFunctionQC.java
@@ -7,7 +7,7 @@ import edu.sc.seis.receiverFunction.hibernate.ReceiverFunctionResult;
 public class UserReceiverFunctionQC {
 
     protected long dbid;
-    
+
     protected QCUser user;
 
     protected ReceiverFunctionResult rfResult;
@@ -15,4 +15,44 @@ public class UserReceiverFunctionQC {
     protected String reason;
 
     protected Timestamp insertTime;
+
+    public long getDbid() {
+        return dbid;
+    }
+
+    public QCUser getUser() {
+        return user;
+    }
+
+    public ReceiverFunctionResult getRfResult() {
+        return rfResult;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public Timestamp getInsertTime() {
+        return insertTime;
+    }
+
+    protected void setDbid(long dbid) {
+        this.dbid = dbid;
+    }
+
+    protected void setUser(QCUser user) {
+        this.user = user;
+    }
+
+    protected void setRfResult(ReceiverFunctionResult rfResult) {
+        this.rfResult = rfResult;
+    }
+
+    protected void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    protected void setInsertTime(Timestamp insertTime) {
+        this.insertTime = insertTime;
+    }
 }

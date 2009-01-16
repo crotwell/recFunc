c5b3257b5e7fbe51efb6ab73501697be5645eb22
crotwell <crotwell@localhost>
2009-01-16 09:44:10 -0500

    [svn r20163] add email, constructor

--- src/edu/sc/seis/receiverFunction/QCUser.java
+++ src/edu/sc/seis/receiverFunction/QCUser.java
@@ -1,14 +1,24 @@
 package edu.sc.seis.receiverFunction;
 
 public class QCUser {
+
     /** for hibernate */
     protected QCUser() {}
 
+    public QCUser(String username, String passwordHash, String email) {
+        super();
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.email = email;
+    }
+
     protected long dbid;
-    
+
     protected String username;
 
     protected String passwordHash;
+
+    protected String email;
 
     protected void setUsername(String username) {
         this.username = username;
@@ -32,5 +42,13 @@ public class QCUser {
 
     protected void setDbid(long dbid) {
         this.dbid = dbid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    protected void setEmail(String email) {
+        this.email = email;
     }
 }

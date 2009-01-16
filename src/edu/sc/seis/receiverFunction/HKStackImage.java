dfb0d3f05145c29f771cd868835b3f58d44e2396
crotwell <crotwell@localhost>
2009-01-16 09:52:53 -0500

    [svn r20168] url for cpt file, reindent

--- src/edu/sc/seis/receiverFunction/HKStackImage.java
+++ src/edu/sc/seis/receiverFunction/HKStackImage.java
@@ -1,9 +1,8 @@
 /**
  * HKStackImage.java
- *
+ * 
  * @author Created by Omnicore CodeGuide
  */
-
 package edu.sc.seis.receiverFunction;
 
 import java.awt.Color;
@@ -13,6 +12,9 @@ import java.awt.Graphics2D;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 import javax.swing.JComponent;
@@ -31,18 +33,17 @@ public class HKStackImage extends JComponent {
     }
 
     HKStackImage(HKStack stack, String phase, int smallestHindex) {
-        CPT_FILE = getCPTFile();
         this.stack = stack;
         this.smallestHindex = smallestHindex;
-        if (phase.equals("all")) {
+        if(phase.equals("all")) {
             stackOut = stack.getStack();
         } else {
             CmplxArray2D analytic = null;
-            if (phase.equals("Ps")) {
+            if(phase.equals("Ps")) {
                 analytic = stack.getAnalyticPs();
-            } else if (phase.equals("PpPs")) {
+            } else if(phase.equals("PpPs")) {
                 analytic = stack.getAnalyticPpPs();
-            } else if (phase.equals("PsPs")) {
+            } else if(phase.equals("PsPs")) {
                 analytic = stack.getAnalyticPsPs();
             }
             stackOut = new float[analytic.getXLength()][analytic.getYLength()];
@@ -52,18 +53,17 @@ public class HKStackImage extends JComponent {
                 }
             }
         }
-        Dimension imageSize = new Dimension(2*stackOut[0].length, 2*(stackOut.length-smallestHindex));
+        Dimension imageSize = new Dimension(2 * stackOut[0].length,
+                                            2 * (stackOut.length - smallestHindex));
         setMinimumSize(imageSize);
         setPreferredSize(imageSize);
         // color map max and min
         int[] xyMin = stack.getMinValueIndices();
         StackMaximum xyMax = stack.getGlobalMaximum();
-
-        //colorMapMin = stack.stack[xyMin[0]][xyMin[1]];
+        // colorMapMin = stack.stack[xyMin[0]][xyMin[1]];
         colorMapMin = 0;
         colorMapMax = xyMax.getMaxValue();
     }
-    
 
     public void addMarker(StationResult result, Color color) {
         markers.add(new Marker(result, color));
@@ -72,64 +72,63 @@ public class HKStackImage extends JComponent {
     public void paintComponent(Graphics graphics) {
         Graphics2D g = (Graphics2D)graphics;
         Color origColor = g.getColor();
-
         int[] xyMin = stack.getMinValueIndices();
         StackMaximum xyMax = stack.getGlobalMaximum();
-
         float min = stack.stack[xyMin[0]][xyMin[1]];
         float max = xyMax.getMaxValue();
-
-        
         try {
-            BufferedReader buf = new BufferedReader(new FileReader(CPT_FILE));
-            colorPallete = GMTColorPalette.load(buf).renormalize(colorMapMin, colorMapMax, Color.BLACK, Color.MAGENTA, Color.CYAN);
-            
+            BufferedReader buf = new BufferedReader(new InputStreamReader(getCPTFile().openStream()));
+            colorPallete = GMTColorPalette.load(buf).renormalize(colorMapMin,
+                                                                 colorMapMax,
+                                                                 Color.BLACK,
+                                                                 Color.MAGENTA,
+                                                                 Color.CYAN);
             buf.close();
-        } catch (IOException e) {
+        } catch(IOException e) {
             logger.warn(e);
             colorPallete = GMTColorPalette.getDefault(colorMapMin, colorMapMax);
         }
-        for (int j = smallestHindex; j < stackOut.length; j++) {
-            for (int k = 0; k < stackOut[j].length; k++) {
-                if (j== xyMax.getHIndex() && k==xyMax.getKIndex()) {
+        for(int j = smallestHindex; j < stackOut.length; j++) {
+            for(int k = 0; k < stackOut[j].length; k++) {
+                if(j == xyMax.getHIndex() && k == xyMax.getKIndex()) {
                     g.setColor(Color.red);
                 } else {
                     Color color = colorPallete.getColor(stackOut[j][k]);
                     g.setColor(color);
                 }
-                g.fillRect( 2*k, 2*(j-smallestHindex), 2, 2);
+                g.fillRect(2 * k, 2 * (j - smallestHindex), 2, 2);
             }
         }
-        
         // tradeoff curves
         /*
-        g.setColor(Color.CYAN);
-        TradeoffCurve tradeoff = new TradeoffCurve(stack);
-        float[] curve = tradeoff.getH_Ps();
-        for (int k = 0; k < stackOut[0].length-1; k++) {
-            g.drawLine(2*k, Math.round(2*stack.getHIndexFloat(new QuantityImpl(curve[k], UnitImpl.KILOMETER))),
-                       2*(k+1), 2*stack.getHIndex(new QuantityImpl(curve[k+1], UnitImpl.KILOMETER)));
-        }
-        curve = tradeoff.getH_PpPs();
-        for (int k = 0; k < stackOut[0].length-1; k++) {
-            g.drawLine(2*k, Math.round(2*stack.getHIndexFloat(new QuantityImpl(curve[k], UnitImpl.KILOMETER))),
-                       2*(k+1), 2*stack.getHIndex(new QuantityImpl(curve[k+1], UnitImpl.KILOMETER)));
-        }
-        curve = tradeoff.getH_PsPs();
-        for (int k = 0; k < stackOut[0].length-1; k++) {
-            g.drawLine(2*k, Math.round(2*stack.getHIndexFloat(new QuantityImpl(curve[k], UnitImpl.KILOMETER))),
-                       2*(k+1), 2*stack.getHIndex(new QuantityImpl(curve[k+1], UnitImpl.KILOMETER)));
-        }*/
-        
+         * g.setColor(Color.CYAN); TradeoffCurve tradeoff = new
+         * TradeoffCurve(stack); float[] curve = tradeoff.getH_Ps(); for (int k =
+         * 0; k < stackOut[0].length-1; k++) { g.drawLine(2*k,
+         * Math.round(2*stack.getHIndexFloat(new QuantityImpl(curve[k],
+         * UnitImpl.KILOMETER))), 2*(k+1), 2*stack.getHIndex(new
+         * QuantityImpl(curve[k+1], UnitImpl.KILOMETER))); } curve =
+         * tradeoff.getH_PpPs(); for (int k = 0; k < stackOut[0].length-1; k++) {
+         * g.drawLine(2*k, Math.round(2*stack.getHIndexFloat(new
+         * QuantityImpl(curve[k], UnitImpl.KILOMETER))), 2*(k+1),
+         * 2*stack.getHIndex(new QuantityImpl(curve[k+1], UnitImpl.KILOMETER))); }
+         * curve = tradeoff.getH_PsPs(); for (int k = 0; k <
+         * stackOut[0].length-1; k++) { g.drawLine(2*k,
+         * Math.round(2*stack.getHIndexFloat(new QuantityImpl(curve[k],
+         * UnitImpl.KILOMETER))), 2*(k+1), 2*stack.getHIndex(new
+         * QuantityImpl(curve[k+1], UnitImpl.KILOMETER))); }
+         */
         Iterator it = markers.iterator();
         while(it.hasNext()) {
             Marker mark = (Marker)it.next();
             g.setColor(mark.getColor());
-            g.fillRect(Math.round(2*stack.getKIndexFloat(mark.getVpvs())), Math.round(2*stack.getHIndexFloat(mark.getDepth())), 2, 2);
+            g.fillRect(Math.round(2 * stack.getKIndexFloat(mark.getVpvs())),
+                       Math.round(2 * stack.getHIndexFloat(mark.getDepth())),
+                       2,
+                       2);
         }
         g.setColor(origColor);
     }
-    
+
     public GMTColorPalette getColorPallete() {
         return colorPallete;
     }
@@ -138,43 +137,50 @@ public class HKStackImage extends JComponent {
         return colorMapMax;
     }
 
-    
     public void setColorMapMax(float colorMapMax) {
         this.colorMapMax = colorMapMax;
     }
 
-    
     public float getColorMapMin() {
         return colorMapMin;
     }
 
-    
     public void setColorMapMin(float colorMapMin) {
         this.colorMapMin = colorMapMin;
     }
-    
-    public static String getCPTFile() {
-        return System.getProperty("gmt.cpt_file", DEFAULT_CPT_FILE);
+
+    public static URL getCPTFile() {
+        if(CPT_FILE == null) {
+            String prop = System.getProperty("gmt.cpt_file");
+            if(prop != null && prop != "") {
+                try {
+                    CPT_FILE = new URL(prop);
+                } catch(MalformedURLException e) {
+                    throw new RuntimeException("Shouldn't happen", e);
+                }
+            } else {
+                CPT_FILE = DEFAULT_CPT_FILE;
+            }
+        }
+        return CPT_FILE;
     }
-    
-    String CPT_FILE;
-    
-    static final String DEFAULT_CPT_FILE = "site/GMT_wysiwyg.cpt";
-    
+
+    static URL CPT_FILE;
+
+    static final URL DEFAULT_CPT_FILE = HKStackImage.class.getClassLoader()
+            .getResource("edu/sc/seis/receiverFunction/GMT_wysiwyg.cpt");
+
     GMTColorPalette colorPallete;
-    
+
     protected HKStack stack;
-    
+
     float[][] stackOut;
-    
+
     float colorMapMax, colorMapMin;
 
     protected ArrayList markers = new ArrayList();
 
     protected int smallestHindex = 0;
-    
+
     private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(HKStackImage.class);
-
-    
 }
-
--- src/edu/sc/seis/receiverFunction/web/HKLatLonPlot.java
+++ src/edu/sc/seis/receiverFunction/web/HKLatLonPlot.java
@@ -5,6 +5,7 @@ import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.sql.SQLException;
@@ -123,7 +124,7 @@ public class HKLatLonPlot extends HttpServlet {
         }
         GMTColorPalette colorPallete;
         try {
-            BufferedReader buf = new BufferedReader(new FileReader(HKStackImage.getCPTFile()));
+            BufferedReader buf = new BufferedReader(new InputStreamReader(HKStackImage.getCPTFile().openStream()));
             colorPallete = GMTColorPalette.load(buf).renormalize(minZ, maxZ, Color.BLACK, Color.MAGENTA, Color.CYAN);
             buf.close();
         } catch (IOException e) {

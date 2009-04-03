38128e03616d97771b9c67de4f60e263401a58af
crotwell <crotwell@localhost>
2009-04-03 15:54:47 -0400

    [svn r20411] name instead of username

--- src/edu/sc/seis/receiverFunction/QCUser.java
+++ src/edu/sc/seis/receiverFunction/QCUser.java
@@ -1,35 +1,43 @@
 package edu.sc.seis.receiverFunction;
 
 public class QCUser {
-
     /** for hibernate */
     protected QCUser() {}
 
-    public QCUser(String username, String passwordHash, String email) {
+    public QCUser(String name, String passwordHash, String email) {
         super();
-        this.username = username;
+        this.name = name;
         this.passwordHash = passwordHash;
         this.email = email;
     }
-
+    
     protected long dbid;
-
-    protected String username;
+    
+    protected String name;
 
     protected String passwordHash;
-
+    
     protected String email;
 
-    protected void setUsername(String username) {
-        this.username = username;
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    protected void setEmail(String email) {
+        this.email = email;
+    }
+
+    protected void setName(String name) {
+        this.name = name;
     }
 
     protected void setPasswordHash(String passwordHash) {
         this.passwordHash = passwordHash;
     }
 
-    public String getUsername() {
-        return username;
+    public String getName() {
+        return name;
     }
 
     public String getPasswordHash() {
@@ -44,11 +52,4 @@ public class QCUser {
         this.dbid = dbid;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    protected void setEmail(String email) {
-        this.email = email;
-    }
 }
